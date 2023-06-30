a7c642b1486f533417ed30fad4f40aa71597212f
rsmaciel <rsmaciel@recordtv.com.br>
2023-06-30 11:36:29 -0300

    criação do básico do admin/dashboard para votação

---
+++ apps/admin/src/app/app.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react'
+import { Link, Route, Routes } from 'react-router-dom'
+
+const Voting = React.lazy(() => import('voting/Module'))
+
+export function App() {
+  return (
+    <React.Suspense fallback={null}>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+
+        <li>
+          <Link to="/voting">Voting</Link>
+        </li>
+      </ul>
+      <Routes>
+        <Route path="/" element={<h1>Hello Dashboard</h1>} />
+
+        <Route path="/voting" element={<Voting />} />
+      </Routes>
+    </React.Suspense>
+  )
+}
+
+export default App
---
+++ apps/voting/src/app/app.tsx
@@ -0,0 +1,9 @@
+export function App() {
+  return (
+    <div>
+      <h2>Votação</h2>
+    </div>
+  )
+}
+
+export default App
