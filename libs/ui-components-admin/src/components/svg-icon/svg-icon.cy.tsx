a6252b099270364b0fbbf07b4ab22641f233ccbc
rafaelfs96 <rafaelfs96@outlook.com>
2023-07-14 15:54:48 -0300

    correcao teste

--- libs/ui-components-admin/src/components/svg-icon/svg-icon.cy.tsx
+++ libs/ui-components-admin/src/components/svg-icon/svg-icon.cy.tsx
@@ -3,6 +3,7 @@ import { SvgIcon } from './svg-icon'
 describe(SvgIcon.name, () => {
   it('render icon with default size', () => {
     cy.mount(<SvgIcon iconName="github" />)
+    cy.get('svg').should('be.visible')
     cy.matchImage()
   })
 })
