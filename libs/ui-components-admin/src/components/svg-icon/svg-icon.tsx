0aa0ec46c624544f66a7b45be622334819427d5d
rafaelfs96 <rafaelfs96@outlook.com>
2023-07-20 13:19:26 -0300

    variacoes de tamanhos para icones possibilidade de opcoes customizadas

--- libs/ui-components-admin/src/components/svg-icon/svg-icon.tsx
+++ libs/ui-components-admin/src/components/svg-icon/svg-icon.tsx
@@ -7,18 +7,12 @@ export type IconProps = {
 }
 
 export function SvgIcon({ iconName, size = 'default', svgProp }: IconProps) {
-  /**
-   * Todo: Variações de formatos
-   * Todo: Testes
-   * Todo: Storybook
-   */
-
   const { loading, error, SvgIconEl } = useDynamicSvgImport(iconName)
 
   const sizesVariant = {
-    small: '',
-    medium: '',
-    default: '',
+    default: { width: 32, height: 32 },
+    medium: { width: 24, height: 24 },
+    small: { width: 16, height: 16 },
   }[size]
 
   return (
@@ -26,7 +20,13 @@ export function SvgIcon({ iconName, size = 'default', svgProp }: IconProps) {
       {loading && <div>...</div>} {/** componente loading */}
       {error && <div>Erro ao carregar icone</div>} {/** componente error */}
       {SvgIconEl && (
-        <SvgIconEl viewBox="0 0 50 50" className={`${sizesVariant}`.trim()} {...svgProp} />
+        <SvgIconEl
+          width={sizesVariant.width}
+          height={sizesVariant.height}
+          {...svgProp}
+          className={`pointer-events-none ${svgProp?.className}`.trim()}
+          viewBox="0 0 50 50"
+        />
       )}
     </>
   )
