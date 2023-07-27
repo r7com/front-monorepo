ea85c3f27a01ee0a742ba13cf95e04f6b0e52d6a
abcosta <abcosta@recordtv.com.br>
2023-07-27 16:47:58 -0300

    criado todas as variacoes

--- apps/voting/src/app/app.tsx
+++ apps/voting/src/app/app.tsx
@@ -6,20 +6,10 @@ export function App() {
     <div>
       <h2 className="text-gray-600 text-lg">Welcome voting</h2>
       <div className="flex justify-center h-screen items-center">
-        <Tooltip content={<Teste />} position="bottom">
-          <Button>anderson batists</Button>
+        <Tooltip content={'batista da cpsta staa'} position="left" arrow>
+          <Button size="default">anderson batists</Button>
         </Tooltip>
       </div>
-    </div>
-  )
-}
-
-function Teste() {
-  return (
-    <div>
-      <h1>asnjsda</h1>
-      <p> anc odb dobdfs pfdns;l pfds pdf pfdsol xxzcxzs</p>
-      <p> anc odb dobdfs pfdns;l pfds pdf pfdsol xxzcxzs</p>
     </div>
   )
 }
--- libs/ui-components-admin/src/components/tooltip/tooltip.tsx
+++ libs/ui-components-admin/src/components/tooltip/tooltip.tsx
@@ -1,10 +1,11 @@
-import { Children, useState } from 'react'
+import { useState } from 'react'
 
 export type TooltipProps<C extends React.ElementType> = {
   content: string | React.ReactNode
   children: React.ReactNode
   position?: 'right' | 'left' | 'bottom' | 'top'
   className?: string
+  arrow?: boolean
 } & React.ComponentPropsWithoutRef<C>
 
 export function Tooltip<C extends React.ElementType>({
@@ -12,31 +13,40 @@ export function Tooltip<C extends React.ElementType>({
   children,
   position = 'top',
   className = '',
+  arrow = true,
 }: TooltipProps<C>) {
   const [isHover, setHover] = useState(false)
-  console.log(Children)
+
   const variant = {
-    top: 'left-1/2 -translate-y-[calc(100%_+_15px)] -translate-x-1/2 after:border-[15px] after:border-solid after:border-transparent after:border-b-[0px] after:border-t-[15px] after:border-t-solid after:border-t-neutral-low-600 after:left-1/2 after:top-full after:-translate-x-1/2',
+    top: 'left-1/2 -translate-y-[calc(100%_+_5px)] -translate-x-1/2 after:border-[5px] after:border-solid after:border-transparent after:border-b-[0px] after:border-t-[5px] after:border-t-solid after:border-t-neutral-low-600 after:left-1/2 after:top-full after:-translate-x-1/2',
     right:
-      '-right-[15px] top-1/2 translate-x-[100%] -translate-y-1/2 after:border-[15px] after:border-solid after:border-transparent after:border-l-[0px] after:border-r-[15px] after:border-r-solid after:border-r-neutral-low-600 after:top-1/2 after:left-[1px] after:-translate-x-full after:-translate-y-1/2',
-    left: 'after:right-0 after:border-[15px] after:border-solid after:border-transparent after:left-1/2 after:border-r-[0px] after:border-l-[15px] after:border-l-neutral-low-600 right-full mr-xxxs top-1/2 -translate-y-1/2 after:translate-x-full after:-translate-y-1/2 after:top-1/2',
+      '-right-[5px] top-1/2 translate-x-[100%] -translate-y-1/2 after:border-[5px] after:border-solid after:border-transparent after:border-l-[0px] after:border-r-[5px] after:border-r-solid after:border-r-neutral-low-600 after:top-1/2 after:left-[1px] after:-translate-x-full after:-translate-y-1/2',
+    left: 'after:right-0 after:border-[5px] after:border-solid after:border-transparent after:left-1/2 after:border-r-[0px] after:border-l-[5px] after:border-l-neutral-low-600 right-[calc(100%_+_5px)] top-1/2 -translate-y-1/2 after:translate-x-full after:-translate-y-1/2 after:top-1/2',
     bottom:
-      'top-[calc(100%_+_15px)] translate-y-0 translate-x-1/2 right-1/2 after:border-[15px] after:border-solid after:border-transparent after:border-t-[0px] after:border-b-[15px] after:border-b-solid after:border-b-neutral-low-600 after:left-1/2 after:bottom-full after:-translate-x-1/2',
+      'top-[calc(100%_+_5px)] translate-y-0 translate-x-1/2 right-1/2 after:border-[5px] after:border-solid after:border-transparent after:border-t-[0px] after:border-b-[5px] after:border-b-solid after:border-b-neutral-low-600 after:left-1/2 after:bottom-full after:-translate-x-1/2',
   }[position]
+
+  const compiledClasses = [
+    variant,
+    arrow ? "after:content-[''] after:block after:absolute" : '',
+    className,
+  ]
+    .join(' ')
+    .trim()
 
   return (
     <div className="group inline relative">
       <span
         role="tooltip"
+        id="tooltip"
         onMouseEnter={() => setHover(false)}
         onMouseLeave={() => setHover(true)}
         aria-hidden={isHover}
-        className={`bg-dark-low-400 w-max hidden group-hover:inline-block text-neutral-high-400 rounded-md absolute px-xxxs py-xxs after:transform after:block after:absolute after:content-[''] ${variant} ${className}`}
+        className={`bg-dark-low-400 opacity-[0] -z-10 w-max group-hover:opacity-[1] group-hover:z-10 rounded-md transition-opacity ease-out duration-300 text-neutral-high-400 absolute px-xxxs py-xxs ${compiledClasses}`}
       >
         {content}
       </span>
-
-      {children}
+      <div aria-describedby="tooltip">{children}</div>
     </div>
   )
 }
