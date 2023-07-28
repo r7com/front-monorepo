0e4add26404ba5e282e4bec80ecdd10b23c3093b
Kelly Guimarans <kguimarans@recordtv.com.br>
2023-07-28 11:53:59 -0300

    feat: Desenvolvimento do componente de status

---
+++ libs/ui-components-admin/src/components/card-status/card-status.tsx
@@ -0,0 +1,35 @@
+import { SvgIcon } from '../svg-icon'
+
+export type CardStatusProps = {
+  text: string
+  variant: 'success' | 'error' | 'helper' | 'info'
+}
+
+export function CardStatus({ text, variant }: CardStatusProps) {
+  const statusIconVariant = {
+    success: 'fill-feedback-success-400',
+    error: 'fill-feedback-warning-400',
+    helper: 'fill-feedback-helper-400',
+    info: 'fill-brand-primary-400',
+  }[variant]
+
+  const statusTextVariant = {
+    success: 'text-feedback-success-400',
+    error: 'text-feedback-warning-400',
+    helper: 'text-feedback-helper-400',
+    text: 'text-feedback-success-400',
+    info: 'text-brand-primary-400',
+  }[variant]
+
+  const compiledClasses = [statusIconVariant, statusTextVariant].join(' ').trim()
+
+  return (
+    <div className={`${variant} flex items-center p-nano`}>
+      <SvgIcon
+        iconName={variant}
+        className={`${compiledClasses} w-xxxs h-xxxs min-w-max min-h-max m-nano`}
+      />
+      <p className={`text-xxxs ${compiledClasses}`}>{text}</p>
+    </div>
+  )
+}
