475f61ac0e6f7ba36a8b248ee0f91b77de14ac80
Kelly Guimarans <kguimarans@recordtv.com.br>
2023-08-01 12:53:29 -0300

    fix: inclusão de tag para teste

--- libs/ui-components-admin/src/components/card-status/card-status.tsx
+++ libs/ui-components-admin/src/components/card-status/card-status.tsx
@@ -24,7 +24,7 @@ export function CardStatus({ text, variant }: CardStatusProps) {
   const compiledClasses = [statusIconVariant, statusTextVariant].join(' ').trim()
 
   return (
-    <div className="flex items-center p-nano">
+    <div data-test="card-status" className="flex items-center p-nano">
       <SvgIcon
         iconName={variant}
         className={`${compiledClasses} w-xxxs h-xxxs min-w-max min-h-max m-nano`}
