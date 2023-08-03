2c334cffb187f5e33ae11361a6f10c3c16982f3c
Gabriel Shioda <gshlima@recordtv.com.br>
2023-08-03 13:33:19 -0300

    Feita a troca dos seletores para data-testid

--- libs/ui-components-admin/src/components/bullet/bullet.cy.tsx
+++ libs/ui-components-admin/src/components/bullet/bullet.cy.tsx
@@ -1,32 +1,34 @@
 import { Bullet } from './bullet'
 
+const bulletSelector = '[data-testid="bullet"]'
+
 describe(Bullet.name, () => {
   const text = 'Bullet'
   it(`check status published`, () => {
     cy.mount(<Bullet text={text} variant="published" />)
-    cy.get('[data-testid="bullet"]').should('be.visible')
-    cy.get('[data-testid="bullet"] > p').should('have.text', text)
+    cy.get(`${bulletSelector}`).should('be.visible')
+    cy.get(`${bulletSelector} > p`).should('have.text', text)
     cy.matchImage()
   })
 
   it(`check status waiting`, () => {
     cy.mount(<Bullet text={text} variant="waiting" />)
-    cy.get('[data-testid="bullet"]').should('be.visible')
-    cy.get('[data-testid="bullet"] > p').should('have.text', text)
+    cy.get(`${bulletSelector}`).should('be.visible')
+    cy.get(`${bulletSelector} > p`).should('have.text', text)
     cy.matchImage()
   })
 
   it(`check status closed`, () => {
     cy.mount(<Bullet text={text} variant="closed" />)
-    cy.get('[data-testid="bullet"]').should('be.visible')
-    cy.get('[data-testid="bullet"] > p').should('have.text', text)
+    cy.get(`${bulletSelector}`).should('be.visible')
+    cy.get(`${bulletSelector} > p`).should('have.text', text)
     cy.matchImage()
   })
 
   it(`check status paused`, () => {
     cy.mount(<Bullet text={text} variant="paused" />)
-    cy.get('[data-testid="bullet"]').should('be.visible')
-    cy.get('[data-testid="bullet"] > p').should('have.text', text)
+    cy.get(`${bulletSelector}`).should('be.visible')
+    cy.get(`${bulletSelector} > p`).should('have.text', text)
     cy.matchImage()
   })
 })
