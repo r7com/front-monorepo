815fc76e93df1d65994a8bd95b782a2c25b5e937
willian <whira@recordtv.com.br>
2023-08-03 17:39:13 -0300

    feat: adicionando estados de hover e pressed para o componente de toggle

--- libs/ui-components-admin/src/components/toggle/toggle.tsx
+++ libs/ui-components-admin/src/components/toggle/toggle.tsx
@@ -9,12 +9,15 @@ export type ToggleProps = {
 
 export function Toggle({ status, size, enabled, children }: ToggleProps) {
   return (
-    <label className="relative flex items-center p-2 text-xl">
+    <label className="relative flex items-center group p-2 text-xl">
       <input
         type="checkbox"
         className="absolute -translate-x-1/2 w-full h-full peer appearance-none rounded-md"
       />
-      <span className="w-xxxl h-sm flex items-center flex-shrink-0 p-1 bg-brand-primary-500 rounded-pill p-quark duration-300 ease-in-out peer-checked:bg-highlight-400 after:w-xs after:h-xs after:bg-light-high-500 after:rounded-circular after:shadow-md after:duration-300 peer-checked:after:translate-x-sm"></span>
+      <span
+        className="w-xxxl h-sm flex items-center flex-shrink-0 p-1 bg-dark-high-500 rounded-pill p-quark duration-300 ease-in-out group-hover:bg-dark-high-600 group-active:shadow-neutral-low-600
+      group-active:shadow-inner peer-checked:bg-brand-primary-500 group-hover:peer-checked:bg-brand-primary-600 group-active:peer-checked:shadow-neutral-low-600 group-active:peer-checked:shadow-inner after:w-xs after:h-xs after:bg-light-high-500 after:rounded-circular after:shadow-md after:duration-300 peer-checked:after:translate-x-sm"
+      ></span>
     </label>
   )
 }
