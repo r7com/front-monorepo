e45c67eea9da026c5a68474f41da084d010641b6
willian <whira@recordtv.com.br>
2023-08-07 17:08:21 -0300

    test: adicionando testes para o componente de toggle

---
+++ libs/ui-components-admin/src/components/toggle/toggle.cy.tsx
@@ -0,0 +1,54 @@
+import { Toggle } from './toggle'
+
+const toggleSelector = '[data-testid="toggle"]'
+
+describe(Toggle.name, () => {
+  it('render default toggle', () => {
+    cy.mount(<Toggle></Toggle>)
+
+    cy.get(toggleSelector).should('be.visible')
+    cy.matchImage()
+  })
+
+  it('render large toggle', () => {
+    cy.mount(<Toggle size="large"></Toggle>)
+
+    cy.get(toggleSelector).should('be.visible')
+    cy.matchImage()
+  })
+
+  it('render medium toggle', () => {
+    cy.mount(<Toggle size="medium"></Toggle>)
+
+    cy.get(toggleSelector).should('be.visible')
+    cy.matchImage()
+  })
+
+  it('render small toggle', () => {
+    cy.mount(<Toggle size="small"></Toggle>)
+
+    cy.get(toggleSelector).should('be.visible')
+    cy.matchImage()
+  })
+
+  it('render checked toggle', () => {
+    cy.mount(<Toggle checked></Toggle>)
+
+    cy.get(toggleSelector).should('be.visible')
+    cy.matchImage()
+  })
+
+  it('render unchecked toggle', () => {
+    cy.mount(<Toggle checked={false}></Toggle>)
+
+    cy.get(toggleSelector).should('be.visible')
+    cy.matchImage()
+  })
+
+  it('render disabled toggle', () => {
+    cy.mount(<Toggle disabled></Toggle>)
+
+    cy.get(toggleSelector).should('be.visible')
+    cy.matchImage()
+  })
+})
