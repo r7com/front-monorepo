84d675e7c0052bc0e4bf5f10d7318391aff0b1c6
Gabriel Shioda <gshlima@recordtv.com.br>
2023-08-08 15:42:59 -0300

    Ajuste no storybook do avatar

--- libs/ui-components-admin/src/components/avatar/avatar.stories.tsx
+++ libs/ui-components-admin/src/components/avatar/avatar.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
-import { Avatar } from '.'
+import { Avatar } from './avatar'
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 const meta: Meta<typeof Avatar> = {
@@ -15,6 +15,11 @@ type Story = StoryObj<typeof Avatar>
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Primary: Story = {
+  render: ({ ...args }) => (
+    <div>
+      <Avatar {...args} />
+    </div>
+  ),
   args: {
     type: 'default',
     sourceUrl: '',
