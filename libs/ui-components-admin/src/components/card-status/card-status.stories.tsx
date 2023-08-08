c9a0fa4da5781f6071c2678ed4da344a99c5c16b
Gabriel Shioda <gshlima@recordtv.com.br>
2023-08-08 15:46:06 -0300

    Ajustes no storybook do card-status

--- libs/ui-components-admin/src/components/card-status/card-status.stories.tsx
+++ libs/ui-components-admin/src/components/card-status/card-status.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
-import { CardStatus } from '.'
+import { CardStatus } from './card-status'
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 const meta: Meta<typeof CardStatus> = {
@@ -15,6 +15,11 @@ type Story = StoryObj<typeof CardStatus>
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Status: Story = {
+  render: ({ ...args }) => (
+    <div>
+      <CardStatus {...args} />
+    </div>
+  ),
   args: {
     text: 'Card status',
     variant: 'success',
