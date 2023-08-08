a831d1cbd9279b411baf7328e02ddf5b63b1da88
willian <whira@recordtv.com.br>
2023-08-08 17:19:34 -0300

    doc: adicionando Toggle ao storybook

---
+++ libs/ui-components-admin/src/components/toggle/toggle.stories.tsx
@@ -0,0 +1,34 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Toggle } from './toggle'
+
+const meta: Meta<typeof Toggle> = {
+  title: 'Example/Toggle',
+  component: Toggle,
+  tags: ['autodocs'],
+  argTypes: {},
+}
+
+export default meta
+
+type Story = StoryObj<typeof Toggle>
+
+export const Primary: Story = {
+  render: ({ ...args }) => (
+    <div>
+      <Toggle {...args} />
+    </div>
+  ),
+  args: {
+    size: 'large',
+    checked: false,
+    disabled: false,
+  },
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/EMH8xJPwcYWJYP8DBYePpR/Desktop%3A-Votação?type=design&node-id=1108%3A74&mode=dev',
+      accessToken: 'figd_sHs6Ap894w4C-OAFls7tuq0fMMROyue-8zQJ8hRE',
+    },
+  },
+}
