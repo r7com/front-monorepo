2f7fb89118e79b235bbdb846ae96444e3fed85c7
willian <whira@recordtv.com.br>
2023-08-11 12:57:12 -0300

    doc: ajuste no storybook do toggle

--- libs/ui-components-admin/src/components/toggle/toggle.stories.tsx
+++ libs/ui-components-admin/src/components/toggle/toggle.stories.tsx
@@ -13,7 +13,7 @@ export default meta
 
 type Story = StoryObj<typeof Toggle>
 
-export const Primary: Story = {
+export const Default: Story = {
   render: ({ ...args }) => (
     <div>
       <Toggle {...args} />
