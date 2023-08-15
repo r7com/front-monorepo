d6039a6a75f481e8c3b87b2ad8ab34488c18f8f6
rafaelfs96 <rafaelfs96@outlook.com>
2023-08-15 12:27:03 -0300

    fix: correção na sombra dos botoes quando focados

--- libs/ui-components-admin/src/components/button/button.tsx
+++ libs/ui-components-admin/src/components/button/button.tsx
@@ -32,7 +32,7 @@ export function Button<C extends React.ElementType = 'button'>({
     text-center
     leading-6
     font-medium
-    transition-colors
+    transition-all
     ${disabled ? '!pointer-events-none !select-none' : ''}
   `
 
@@ -45,17 +45,17 @@ export function Button<C extends React.ElementType = 'button'>({
   const colorsVariant = {
     primary: `
       bg-brand-primary-500 hover:bg-brand-primary-600 active:bg-brand-primary-400
-      border-brand-primary-500 hover:border-brand-primary-600 active:border-brand-primary-600
+      border-brand-primary-500 hover:border-brand-primary-600 active:border-brand-primary-600 focus:border-light-high-500
       text-light-high-500 active:text-brand-primary-600
     `,
     secondary: `
       bg-highlight-500 hover:bg-highlight-600 active:bg-highlight-400
-      border-highlight-500 hover:border-highlight-600 active:border-highlight-600
+      border-highlight-500 hover:border-highlight-600 active:border-highlight-600 focus:border-light-high-500
       text-light-high-500 active:text-highlight-600
     `,
     tertiary: `
       bg-transparent
-      border-brand-primary-500 hover:border-brand-primary-600 active:border-light-low-500
+      border-brand-primary-500 hover:border-brand-primary-600 active:border-light-low-500 focus:shadow-level1 focus:shadow-brand-primary-500
       text-brand-primary-500 hover:text-brand-primary-600 active:text-light-low-500
     `,
     ghost: `
