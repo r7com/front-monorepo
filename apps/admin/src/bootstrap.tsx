4c5a5fd1ed01f66fc9b14640659e59b94f19e4d6
rafaelfs96 <rafaelfs96@outlook.com>
2023-08-23 14:58:53 -0300

    task: importando provider de notificacao e disponibilizando para uso no admin

--- apps/admin/src/bootstrap.tsx
+++ apps/admin/src/bootstrap.tsx
@@ -3,13 +3,17 @@ import * as ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './style.css'
 
+import { NotificationProvider } from '@r7-front-monorepo/ui-components-admin'
+
 import App from './app/app'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <NotificationProvider>
+        <App />
+      </NotificationProvider>
     </BrowserRouter>
   </StrictMode>,
 )
