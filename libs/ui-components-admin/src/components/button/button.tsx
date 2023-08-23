7c1e9db567543bd068dfbf9231dcbe694107702c
rafaelfs96 <rafaelfs96@outlook.com>
2023-08-23 10:33:27 -0300

    refactor: utilizando tailwind-variants nos componentes: * bullet * button * card-status * svg-icon * tooltip

--- libs/ui-components-admin/src/components/button/button.tsx
+++ libs/ui-components-admin/src/components/button/button.tsx
@@ -1,13 +1,13 @@
+import { variants, ButtonVariants } from './variants'
+
 export type ButtonProps<C extends React.ElementType> = {
   as?: C
   className?: string
-  size?: 'large' | 'medium' | 'small'
-  color?: 'primary' | 'secondary' | 'tertiary' | 'ghost'
-  disabled?: boolean
   startIcon?: React.ReactNode | null
   endIcon?: React.ReactNode | null
   children: React.ReactNode
-} & React.ComponentPropsWithoutRef<C>
+} & ButtonVariants &
+  React.ComponentPropsWithoutRef<C>
 
 export function Button<C extends React.ElementType = 'button'>({
   as,
@@ -22,68 +22,12 @@ export function Button<C extends React.ElementType = 'button'>({
 }: ButtonProps<C>) {
   const DynamicTag = as || 'button'
 
-  const defaultVariant = `
-    inline-flex
-    items-center
-    box-border
-    cursor-pointer
-    border-hairline
-    rounded-md
-    text-center
-    leading-6
-    font-medium
-    transition-all
-    ${disabled ? '!pointer-events-none !select-none' : ''}
-  `
-
-  const sizesVariant = {
-    large: 'text-xs py-[16px] px-nano',
-    medium: 'text-xxs py-[10px] px-nano',
-    small: 'text-xxxs py-[6px] px-nano',
-  }[size]
-
-  const colorsVariant = {
-    primary: `
-      bg-brand-primary-500 hover:bg-brand-primary-600 active:bg-brand-primary-400
-      border-brand-primary-500 hover:border-brand-primary-600 active:border-brand-primary-600 focus:border-light-high-500
-      text-light-high-500 active:text-brand-primary-600
-    `,
-    secondary: `
-      bg-highlight-500 hover:bg-highlight-600 active:bg-highlight-400
-      border-highlight-500 hover:border-highlight-600 active:border-highlight-600 focus:border-light-high-500
-      text-light-high-500 active:text-highlight-600
-    `,
-    tertiary: `
-      bg-transparent
-      border-brand-primary-500 hover:border-brand-primary-600 active:border-light-low-500 focus:shadow-level1 focus:shadow-brand-primary-500
-      text-brand-primary-500 hover:text-brand-primary-600 active:text-light-low-500
-    `,
-    ghost: `
-      bg-transparent
-      border-none
-      text-brand-primary-500 hover:text-brand-primary-600 active:text-light-low-500
-    `,
-  }[color]
-
-  const disabledColorsVariant = {
-    primary: '!text-light-low-400 !border-dark-high-600 !bg-dark-high-600',
-    secondary: '!text-light-low-400 !border-dark-high-600 !bg-dark-high-600',
-    tertiary: '!text-dark-high-500 !border-dark-high-500',
-    ghost: '!text-dark-high-500',
-  }[color]
-
-  const compiledClasses = [
-    defaultVariant,
-    className,
-    sizesVariant,
-    colorsVariant,
-    disabled ? disabledColorsVariant : '',
-  ]
-    .join(' ')
-    .trim()
-
   return (
-    <DynamicTag data-testid="button" className={compiledClasses} {...rest}>
+    <DynamicTag
+      data-testid="button"
+      className={variants({ color, size, disabled, className })}
+      {...rest}
+    >
       {startIcon && (
         <span className="mr-nano" aria-hidden="true">
           {startIcon}
--- libs/ui-components-admin/src/components/tooltip/tooltip.tsx
+++ libs/ui-components-admin/src/components/tooltip/tooltip.tsx
@@ -1,38 +1,20 @@
 import { useState } from 'react'
+import { TooltipVariants, variants } from './variants'
 
 export type TooltipProps = {
   content: string | React.ReactNode
   children: React.ReactNode
-  position?: 'right' | 'left' | 'bottom' | 'top'
   className?: string
-  arrow?: boolean
-}
+} & TooltipVariants
 
 export function Tooltip({
   content,
   children,
-  position = 'top',
   className = '',
+  position = 'top',
   arrow = true,
 }: TooltipProps) {
   const [isHover, setHover] = useState(false)
-
-  const variant = {
-    top: 'left-1/2 -translate-y-[calc(100%_+_5px)] -translate-x-1/2 after:border-[5px] after:border-solid after:border-transparent after:border-b-[0px] after:border-t-[5px] after:border-t-solid after:border-t-neutral-low-600 after:left-1/2 after:top-full after:-translate-x-1/2',
-    right:
-      '-right-[5px] top-1/2 translate-x-[100%] -translate-y-1/2 after:border-[5px] after:border-solid after:border-transparent after:border-l-[0px] after:border-r-[5px] after:border-r-solid after:border-r-neutral-low-600 after:top-1/2 after:left-[1px] after:-translate-x-full after:-translate-y-1/2',
-    left: 'after:right-0 after:border-[5px] after:border-solid after:border-transparent after:left-1/2 after:border-r-[0px] after:border-l-[5px] after:border-l-neutral-low-600 right-[calc(100%_+_5px)] top-1/2 -translate-y-1/2 after:translate-x-full after:-translate-y-1/2 after:top-1/2',
-    bottom:
-      'top-[calc(100%_+_5px)] translate-y-0 translate-x-1/2 right-1/2 after:border-[5px] after:border-solid after:border-transparent after:border-t-[0px] after:border-b-[5px] after:border-b-solid after:border-b-neutral-low-600 after:left-1/2 after:bottom-full after:-translate-x-1/2',
-  }[position]
-
-  const compiledClasses = [
-    variant,
-    arrow ? "after:content-[''] after:block after:absolute" : '',
-    className,
-  ]
-    .join(' ')
-    .trim()
 
   return (
     <div className="inline relative">
@@ -41,9 +23,7 @@ export function Tooltip({
         role="tooltip"
         id="tooltip"
         aria-hidden={!isHover}
-        className={`bg-dark-low-400 w-max ${
-          isHover ? 'opacity-[1] z-10' : 'opacity-[0] -z-10'
-        } rounded-md transition-opacity ease-in-out duration-150 text-neutral-high-400 absolute px-xxxs py-xxs ${compiledClasses}`}
+        className={variants({ position, arrow, hover: isHover, className })}
       >
         {content}
       </span>
