30f0a42465ff2d8d07772dc56ac927d9e5081fc4
Kelly Guimarans <kguimarans@recordtv.com.br>
2023-08-29 12:52:44 -0300

    refactor: ajuste da describe e it do teste

--- libs/ui-components-admin/src/components/card-status/card-status.cy.tsx
+++ libs/ui-components-admin/src/components/card-status/card-status.cy.tsx
@@ -4,28 +4,28 @@ describe(`should render all its ${CardStatus.name}`, () => {
   const text = 'Card Status'
   const al = `[aria-label="status"]`
 
-  it(`Check status of success`, () => {
+  it(`Check render of success`, () => {
     cy.mount(<CardStatus text={text} variant="success" />)
     cy.get(al).should('be.visible')
     cy.findByText(text).should('be.visible')
     cy.matchImage()
   })
 
-  it(`Check status of error`, () => {
+  it(`Check render of error`, () => {
     cy.mount(<CardStatus text={text} variant="error" />)
     cy.get(al).should('be.visible')
     cy.findByText(text).should('be.visible')
     cy.matchImage()
   })
 
-  it(`Check status of helper`, () => {
+  it(`Check render of helper`, () => {
     cy.mount(<CardStatus text={text} variant="helper" />)
     cy.get(al).should('be.visible')
     cy.findByText(text).should('be.visible')
     cy.matchImage()
   })
 
-  it(`Check status of info`, () => {
+  it(`Check render of info`, () => {
     cy.mount(<CardStatus text={text} variant="info" />)
     cy.get(al).should('be.visible')
     cy.findByText(text).should('be.visible')
