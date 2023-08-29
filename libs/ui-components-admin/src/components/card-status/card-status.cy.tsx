95861582fcb2a848ee5f40326b1a0d033b7990c5
Kelly Guimarans <kguimarans@recordtv.com.br>
2023-08-29 11:32:37 -0300

    refactor: ajuste de cypress pelo aria-label

--- libs/ui-components-admin/src/components/card-status/card-status.cy.tsx
+++ libs/ui-components-admin/src/components/card-status/card-status.cy.tsx
@@ -2,38 +2,39 @@ import { CardStatus } from './card-status'
 
 describe(CardStatus.name, () => {
   const text = 'Card Status'
+  const al = `[aria-label="status"]`
 
   it(`Check status of success`, () => {
     cy.mount(<CardStatus text={text} variant="success" />)
-    cy.findByRole('status').should('be.visible')
+    cy.get(al).should('be.visible')
     cy.findByText(text).should('be.visible')
     cy.matchImage()
   })
 
   it(`Check status of error`, () => {
     cy.mount(<CardStatus text={text} variant="error" />)
-    cy.findByRole('status').should('be.visible')
+    cy.get(al).should('be.visible')
     cy.findByText(text).should('be.visible')
     cy.matchImage()
   })
 
   it(`Check status of helper`, () => {
     cy.mount(<CardStatus text={text} variant="helper" />)
-    cy.findByRole('status').should('be.visible')
+    cy.get(al).should('be.visible')
     cy.findByText(text).should('be.visible')
     cy.matchImage()
   })
 
   it(`Check status of info`, () => {
     cy.mount(<CardStatus text={text} variant="info" />)
-    cy.findByRole('status').should('be.visible')
+    cy.get(al).should('be.visible')
     cy.findByText(text).should('be.visible')
     cy.matchImage()
   })
 
   it(`Check status of waiting`, () => {
     cy.mount(<CardStatus text={text} variant="waiting" />)
-    cy.findByRole('status').should('be.visible')
+    cy.get(al).should('be.visible')
     cy.findByText(text).should('be.visible')
     cy.matchImage()
   })
