3f3503480753c1d69ab49f97f7585c29ef7cc080
rafaelfs96 <rafaelfs96@outlook.com>
2023-09-01 10:09:10 -0300

    task: melhorando descricao para testes do snackbar

--- libs/ui-base-components/src/components/snackbar/snackbar.cy.tsx
+++ libs/ui-base-components/src/components/snackbar/snackbar.cy.tsx
@@ -1,25 +1,25 @@
 import { Snackbar } from './snackbar'
 
-describe(Snackbar.name, () => {
-  it('render snackbar informative', () => {
+describe(`${Snackbar.name} variants`, () => {
+  it('should renders snackbar with type: informative', () => {
     cy.mount(<Snackbar message="mensagem" type="informative" open />)
     cy.findByRole('status').should('be.visible')
     cy.matchImage()
   })
 
-  it('render snackbar positive', () => {
+  it('should renders snackbar with type: positive', () => {
     cy.mount(<Snackbar message="mensagem" type="positive" open />)
     cy.findByRole('status').should('be.visible')
     cy.matchImage()
   })
 
-  it('render snackbar alert', () => {
+  it('should renders snackbar with type: alert', () => {
     cy.mount(<Snackbar message="mensagem" type="alert" open />)
     cy.findByRole('status').should('be.visible')
     cy.matchImage()
   })
 
-  it('render snackbar error', () => {
+  it('should renders snackbar with type: error', () => {
     cy.mount(<Snackbar message="mensagem" type="error" open />)
     cy.findByRole('alert').should('be.visible')
     cy.matchImage()
