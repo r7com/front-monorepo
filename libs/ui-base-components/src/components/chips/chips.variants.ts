56aff02d16441caf323ef44d10d19783577c455a
gpesantos <gpesantos@recordtv.com.br>
2023-09-01 10:06:26 -0300

    Ajustes

--- libs/ui-base-components/src/components/chips/chips.variants.ts
+++ libs/ui-base-components/src/components/chips/chips.variants.ts
@@ -1,4 +1,4 @@
-import { createVariants, VariantProps } from '@r7-front-monorepo/utils'
+import { createVariants, VariantProps } from '@r7-jarvics/utils'
 
 export const variants = createVariants({
   base: 'gap-nano px-xxxs rounded-pill font-semibold cursor-pointer flex items-center justify-center w-fit',
