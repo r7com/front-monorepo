8d71c07c7763fae95a40cceea0b298fda25ed69c
rsmaciel <rsmaciel@recordtv.com.br>
2023-09-05 09:57:21 -0300

    trigger button

--- libs/ui-base-components/src/components/button/button.tsx
+++ libs/ui-base-components/src/components/button/button.tsx
@@ -32,7 +32,7 @@ export function Button<C extends React.ElementType = 'button'>({
       {startIcon}
       {children}
       {endIcon}
-      adsfaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+      adsfaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa dsfdfsdfdf
     </DynamicTag>
   )
 }
