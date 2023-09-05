e08354ad0863669c23431c8c74f2a0424c1f0f97
rsmaciel <rsmaciel@recordtv.com.br>
2023-09-05 17:17:16 -0300

    teste cache

--- libs/ui-form-components/src/components/input-field/input-field.cy.tsx
+++ libs/ui-form-components/src/components/input-field/input-field.cy.tsx
@@ -1,6 +1,6 @@
 import { InputField } from './input-field'
 
-describe(`${InputField.name} variant options`, () => {
+describe(InputField.name, () => {
   it('should render as button', () => {
     cy.mount(<InputField name="foo" label="Foo" />)
     cy.findByLabelText(/Foo/i).should('be.visible')
