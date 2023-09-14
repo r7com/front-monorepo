c96e18d987efe9a433c9e07bf2da3e51e7ab8798
Jorge Santos <jldsantos@rederecord.com.br>
2023-09-14 20:42:48 -0300

    refactor: refeito Storybook do Banner com layouts pré-prontos"

--- libs/ui-base-components/src/components/banner/banner.stories.tsx
+++ libs/ui-base-components/src/components/banner/banner.stories.tsx
@@ -1,138 +1,145 @@
 import type { Meta, StoryObj } from '@storybook/react'
 import { Banner } from './'
 import { SvgIcon } from '../svg-icon'
-import { Text } from '../text'
+// import { Text } from '../text'
 import { Button } from '../button'
-import { BannerIconVariants } from '../banner/components/banner-icon-variants'
+import { MockBanner } from './banner'
 import { useArgs } from '@storybook/client-api'
 
-// Unknown temporário
-const meta: Meta<unknown> = {
+const meta: Meta<typeof MockBanner> = {
   title: 'Example/Banner',
-  component: Banner.Root,
+  component: MockBanner,
   tags: ['autodocs'],
+  /** Default em todos os stories */
+  args: {
+    isVisible: true,
+  },
 }
 
 export default meta
 
-type DefaultIconsStory = StoryObj<typeof Banner.Icon>
+type Story = StoryObj<typeof MockBanner>
 
-type CustomStory = StoryObj<typeof Banner.Root>
-
-export const DefaultIcons: DefaultIconsStory = {
-  render: ({ ...args }) => (
-    <div className="p-xxxs w-full max-w-[1084px] mx-auto">
-      <Banner.Root>
-        <Banner.Content>
-          <Banner.Icon type={args.type} />
-
-          <Banner.Description>
-            <Text color="neutralHigh">Fique por dentro das melhores noticias em primeira mão</Text>
-          </Banner.Description>
-        </Banner.Content>
-      </Banner.Root>
-    </div>
-  ),
-  argTypes: {
-    type: {
-      options: ['alert', 'error', 'informative', 'positive'] as Array<BannerIconVariants['type']>,
-      control: { type: 'select' },
-    },
-  },
-  args: {
-    type: 'informative',
-  },
-}
-
-export const DefaultIconsActions: DefaultIconsStory = {
-  render: function Render({ ...args }) {
-    // const [{ checked }, updateArgs] = useArgs()
-    // const handleToggle = () => updateArgs({ checked: !checked })
+export const DefaultIcons: Story = {
+  render: function Render({ ...args }, { globals }) {
+    const [{ isVisible }, updateArgs] = useArgs()
+    const handleToggle = (isVisible: boolean) => updateArgs({ isVisible })
 
     return (
-      <div className="p-xxxs w-full max-w-[1084px] mx-auto">
-        <Banner.Root
-          renderBanner={({ close }) => (
-            <>
-              <Banner.Content>
-                <Banner.Icon type={args.type} />
+      <div className={globals?.backgrounds?.value === '#333333' ? 'dark' : 'light'}>
+        <Banner.Root isVisible={isVisible} onClose={handleToggle}>
+          <Banner.Content>
+            <Banner.Icon type={args.iconType} />
 
-                <Banner.Description>
-                  <Text color="neutralHigh">
-                    Fique por dentro das melhores noticias em primeira mão
-                  </Text>
-                </Banner.Description>
-              </Banner.Content>
-
-              <Banner.Options>
-                <Button>IR PARA</Button>
-                <Button color="ghost" onClick={close}>
-                  Dispensar
-                </Button>
-              </Banner.Options>
-            </>
-          )}
-        />
+            <Banner.Description>{args.description}</Banner.Description>
+          </Banner.Content>
+        </Banner.Root>
       </div>
     )
   },
-  argTypes: {
-    type: {
-      options: ['alert', 'error', 'informative', 'positive'] as Array<BannerIconVariants['type']>,
-      control: { type: 'select' },
-    },
+  args: {
+    iconType: 'informative',
+    description:
+      'O sistema JARVICS será atualizado durante os dias 03 e 04 de agosto de 2023. Contamos com sua compreensão.',
+  },
+  parameters: {
+    controls: { exclude: ['customIconName'] },
+  },
+}
+
+export const CustomIcon: Story = {
+  render: function Render({ ...args }, { globals }) {
+    const [{ isVisible }, updateArgs] = useArgs()
+    const handleToggle = (isVisible: boolean) => updateArgs({ isVisible })
+
+    return (
+      <div className={globals?.backgrounds?.value === '#333333' ? 'dark' : 'light'}>
+        <Banner.Root isVisible={isVisible} onClose={handleToggle}>
+          <Banner.Content>
+            <Banner.Icon type="custom">
+              <SvgIcon iconName={args.customIconName} />
+            </Banner.Icon>
+
+            <Banner.Description>{args.description}</Banner.Description>
+          </Banner.Content>
+        </Banner.Root>
+      </div>
+    )
   },
   args: {
-    type: 'informative',
+    customIconName: 'record',
+    description:
+      'O sistema JARVICS será atualizado durante os dias 03 e 04 de agosto de 2023. Contamos com sua compreensão.',
+  },
+  parameters: {
+    controls: { exclude: ['iconType'] },
   },
 }
 
-export const CustomIcon: CustomStory = {
-  render: ({ ...args }) => (
-    <div className="p-xxxs w-full max-w-[1084px] mx-auto">
-      <Banner.Root>
-        <Banner.Content>
-          <Banner.Icon type="custom">
-            <SvgIcon iconName="record" />
-          </Banner.Icon>
+export const CustomIconsAction: Story = {
+  render: function Render({ ...args }, { globals }) {
+    const [{ isVisible }, updateArgs] = useArgs()
+    const handleToggle = (isVisible: boolean) => updateArgs({ isVisible })
 
-          <Banner.Description>
-            <Text color="neutralHigh">Fique por dentro das melhores noticias em primeira mão</Text>
-          </Banner.Description>
-        </Banner.Content>
-      </Banner.Root>
-    </div>
-  ),
+    return (
+      <div className={globals?.backgrounds?.value === '#333333' ? 'dark' : 'light'}>
+        <Banner.Root isVisible={isVisible}>
+          <Banner.Content>
+            <Banner.Icon type="custom">
+              <SvgIcon iconName={args.customIconName} />
+            </Banner.Icon>
+
+            <Banner.Description>{args.description}</Banner.Description>
+          </Banner.Content>
+
+          <Banner.Options>
+            <Button onClick={() => handleToggle(!isVisible)}>ACEITAR</Button>
+          </Banner.Options>
+        </Banner.Root>
+      </div>
+    )
+  },
+  args: {
+    customIconName: 'record',
+    description:
+      'Este site utiliza cookies e outras tecnologias para melhorar sua experiência. Ao continuar navegando, você aceita as condições de nossa Política de Privacidade',
+  },
+  parameters: {
+    controls: { exclude: ['iconType'] },
+  },
 }
 
-export const CustomIconActions: CustomStory = {
-  render: ({ ...args }) => (
-    <div className="p-xxxs w-full max-w-[1084px] mx-auto">
-      <Banner.Root
-        renderBanner={({ close }) => (
-          <>
-            <Banner.Content>
-              <Banner.Icon type="custom">
-                <SvgIcon iconName="record" />
-              </Banner.Icon>
+export const ImageActions: Story = {
+  render: function Render({ ...args }, { globals }) {
+    const [{ isVisible }, updateArgs] = useArgs()
+    const handleToggle = (isVisible: boolean) => updateArgs({ isVisible })
 
-              <Banner.Description>
-                <Text color="neutralHigh">
-                  Fique por dentro das melhores noticias em primeira mão
-                </Text>
-              </Banner.Description>
-            </Banner.Content>
+    return (
+      <div className={globals?.backgrounds?.value === '#333333' ? 'dark' : 'light'}>
+        <Banner.Root isVisible={isVisible}>
+          <Banner.Content>
+            <Banner.Image alt="Default image" sourceUrl={args.imageSourceUrl} />
 
-            <Banner.Options>
-              <Button>IR PARA</Button>
+            <Banner.Description>{args.description}</Banner.Description>
+          </Banner.Content>
 
-              <Button color="ghost" onClick={close}>
-                Dispensar
-              </Button>
-            </Banner.Options>
-          </>
-        )}
-      />
-    </div>
-  ),
+          <Banner.Options>
+            <Button>CADASTRAR</Button>
+
+            <Button color="ghost" onClick={() => handleToggle(!isVisible)}>
+              Dispensar
+            </Button>
+          </Banner.Options>
+        </Banner.Root>
+      </div>
+    )
+  },
+  args: {
+    description: 'Fique por dentro das melhores noticias em primeira mão',
+    imageSourceUrl:
+      'https://img.r7.com/images/r7-30072019142631584?crop_position=c&dimensions=160x80',
+  },
+  parameters: {
+    controls: { exclude: ['iconType', 'customIconName'] },
+  },
 }
