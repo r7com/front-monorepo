76f2abb380add16663d88d7311fd4c87aa8d8217
Jorge Santos <jldsantos@rederecord.com.br>
2023-09-18 12:35:05 -0300

    style: adiciona min-width 100px no banner-image

--- libs/ui-base-components/src/components/banner/components/banner-image/banner-image.tsx
+++ libs/ui-base-components/src/components/banner/components/banner-image/banner-image.tsx
@@ -8,7 +8,7 @@ export function BannerImage({
   sourceUrl = 'https://img.r7.com/images/r7-30072019142631584?crop_position=c&dimensions=160x80',
 }: BannerImageProps) {
   return (
-    <picture className="flex h-full rounded-lg overflow-hidden">
+    <picture className="w-fit min-w-[100px] h-auto flex rounded-lg overflow-hidden">
       <img className="w-auto h-full" src={sourceUrl} alt={alt} />
     </picture>
   )
