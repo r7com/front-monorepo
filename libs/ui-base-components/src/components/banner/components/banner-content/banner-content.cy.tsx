0efe8be6ac6cd7754e67f37e2e887a63447026b3
Jorge Santos <jldsantos@rederecord.com.br>
2023-09-18 11:45:24 -0300

    test: corrige descrição do it e remove matchImage no banner-content

--- libs/ui-base-components/src/components/banner/components/banner-content/banner-content.cy.tsx
+++ libs/ui-base-components/src/components/banner/components/banner-content/banner-content.cy.tsx
@@ -1,13 +1,12 @@
 import { BannerContent } from './banner-content'
 
 describe(BannerContent.name, () => {
-  it('should render all type variants', () => {
+  it('should render children', () => {
     cy.mount(
       <div className="flex flex-row flex-wrap gap-nano p-nano">
         <BannerContent>Conteúdo do banner</BannerContent>
       </div>,
     )
     cy.findByText('Conteúdo do banner').should('be.visible')
-    cy.matchImage()
   })
 })
