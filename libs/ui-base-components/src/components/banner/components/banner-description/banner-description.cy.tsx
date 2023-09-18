21d30c86bca2833bc94abfe632d4e69f172850d5
Jorge Santos <jldsantos@rederecord.com.br>
2023-09-18 11:39:05 -0300

    test: corrige descrição do it do banner-description

--- libs/ui-base-components/src/components/banner/components/banner-description/banner-description.cy.tsx
+++ libs/ui-base-components/src/components/banner/components/banner-description/banner-description.cy.tsx
@@ -1,7 +1,7 @@
 import { BannerDescription } from './banner-description'
 
 describe(BannerDescription.name, () => {
-  it('should render all type variants', () => {
+  it('should render description', () => {
     cy.mount(
       <div className="flex flex-row flex-wrap gap-nano p-nano">
         <BannerDescription>Descrição do banner</BannerDescription>
