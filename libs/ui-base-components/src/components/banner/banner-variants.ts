b7725fd52898428680b610e95193556e38e330b8
Jorge Santos <jldsantos@rederecord.com.br>
2023-09-18 11:47:15 -0300

    style: corrige dark mode invertido no banner

--- libs/ui-base-components/src/components/banner/banner-variants.ts
+++ libs/ui-base-components/src/components/banner/banner-variants.ts
@@ -1,7 +1,7 @@
 import { createVariants, type VariantProps } from '@r7-jarvics/tools'
 
 export const variants = createVariants({
-  base: 'relative bg-dark-low-400 rounded-lg p-nano flex gap-xxs dark:bg-dark-high-400',
+  base: 'relative rounded-lg p-nano flex gap-xxs bg-dark-high-400 dark:bg-dark-low-400',
   variants: {
     onClose: {
       false: 'max-tablet:flex-wrap',
