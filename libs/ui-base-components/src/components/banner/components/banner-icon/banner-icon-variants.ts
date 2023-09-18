70aa800bd52a4820034a02edc97c3ccbfbc4452e
Jorge Santos <jldsantos@rederecord.com.br>
2023-09-18 11:37:53 -0300

    style: corrige conflito de fill no banner-icon

--- libs/ui-base-components/src/components/banner/components/banner-icon/banner-icon-variants.ts
+++ libs/ui-base-components/src/components/banner/components/banner-icon/banner-icon-variants.ts
@@ -1,16 +1,22 @@
 import { createVariants, type VariantProps } from '@r7-jarvics/tools'
 
 export const variants = createVariants({
-  base: 'w-fit flex items-center justify-center p-xxs rounded-lg [&>svg]:fill-neutral-high-400',
+  base: 'w-fit flex items-center justify-center p-xxs rounded-lg',
   variants: {
     type: {
       informative: 'bg-feedback-informative-500',
       positive: 'bg-feedback-success-500',
       alert: 'bg-feedback-helper-500',
       error: 'bg-feedback-warning-500',
-      custom: 'dark:[&>svg]:fill-dark-low-400',
+      custom: '[&>svg]:fill-dark-low-400 dark:[&>svg]:fill-neutral-high-400',
     },
   },
+  compoundVariants: [
+    {
+      type: ['informative', 'positive', 'alert', 'error'],
+      className: '[&>svg]:fill-neutral-high-400',
+    },
+  ],
 })
 
 export type BannerIconVariants = VariantProps<typeof variants>
