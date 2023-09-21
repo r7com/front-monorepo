300a48cd9b1026a429d86ec185174b2400a607a9
Alescia Fernandes <alfernandes@recordtv.com.br>
2023-09-21 09:19:43 -0300

    refactor: remoção de código desnecessario no componente tab

--- libs/ui-base-components/src/components/tab/tab.tsx
+++ libs/ui-base-components/src/components/tab/tab.tsx
@@ -1,4 +1,3 @@
-import { useRef } from 'react'
 import { SvgIcon, SvgIconProps } from '../svg-icon'
 import { SvgIcons } from '../svg-icon/svg-icon.types'
 import { TabVariants, variants } from './tab.variants'
@@ -7,7 +6,6 @@ export type TabProps = {
   text: string
   id: string
   tabpanelId: string
-  selected?: boolean
   size?: 'small' | 'medium' | 'large'
   startIconName?: SvgIcons
   endIconName?: SvgIcons
@@ -17,12 +15,10 @@ export function Tab({
   text,
   id,
   tabpanelId,
-  selected = false,
   size = 'medium',
   startIconName,
   endIconName,
 }: TabProps) {
-  const ref = useRef<HTMLButtonElement>(null)
   const { button, icon } = variants({ size })
   const tabEvent = new CustomEvent('onTabClick', {
     bubbles: true,
@@ -38,19 +34,16 @@ export function Tab({
 
   function handleClick(event: React.MouseEvent<HTMLButtonElement>) {
     const target = event.target as HTMLElement
-    target.ariaSelected = 'true'
-    target.tabIndex = 0
-    ref.current?.dispatchEvent(tabEvent)
+    target.dispatchEvent(tabEvent)
   }
 
   return (
     <button
-      ref={ref}
       type="button"
       className={button()}
       role="tab"
-      aria-selected={selected}
-      tabIndex={selected ? 0 : -1}
+      aria-selected={false}
+      tabIndex={-1}
       id={id}
       aria-controls={tabpanelId}
       onClick={handleClick}
--- libs/ui-base-components/src/components/tab/tab.variants.ts
+++ libs/ui-base-components/src/components/tab/tab.variants.ts
@@ -4,7 +4,7 @@ export const variants = createVariants({
   slots: {
     button:
       'flex text-light-low-400 p-nano gap-nano items-center border-b-thin border-transparent hover:text-brand-primary-600 group transition-colors active:border-b-brand-primary-500 active:text-light-low-400 active:outline-none active:drop-shadow-none focus-visible:drop-shadow-glow focus-visible:outline-none focus-visible:text-brand-primary-500 focus-visible:border-b-brand-primary-500 aria-selected:text-brand-primary-500 aria-selected:border-b-brand-primary-500',
-    icon: 'fill-light-low-400 group-active:fill-light-low-400  group-hover:fill-brand-primary-600 group-focus-visible:fill-brand-primary-500 group-aria-selected:fill-brand-primary-500',
+    icon: 'fill-light-low-400 group-active:fill-light-low-400  group-hover:fill-brand-primary-600 group-focus-visible:fill-brand-primary-500 group-aria-selected:fill-brand-primary-500 pointer-events-none',
   },
   variants: {
     size: {
