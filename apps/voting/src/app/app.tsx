c78a655846196f01ff6e7b4f67ec356db0647521
abcosta <abcosta@recordtv.com.br>
2023-09-21 12:40:12 -0300

    changes: removed components of application

--- apps/voting/src/app/app.tsx
+++ apps/voting/src/app/app.tsx
@@ -1,11 +1,9 @@
-import { SecondaryFooter } from '@r7-jarvics/ui-footer-delivery'
 import '../style.css'
 
 export function App() {
   return (
     <div>
       <h2 className="text-gray-600 text-lg">Welcome voting</h2>
-      <SecondaryFooter editorialName="Entretenimento" />
     </div>
   )
 }
