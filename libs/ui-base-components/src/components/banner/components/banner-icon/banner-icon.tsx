b886a8eeec0cce6fbb149c3e33c72a9f5f092c8e
Jorge Santos <jldsantos@rederecord.com.br>
2023-09-21 11:05:13 -0300

    refactor: refatora o banner-icon pra sempre ser passado um SvgIcon como children

--- libs/ui-base-components/src/components/banner/components/banner-icon/banner-icon.tsx
+++ libs/ui-base-components/src/components/banner/components/banner-icon/banner-icon.tsx
@@ -1,29 +1,10 @@
-import { SvgIcon, type SvgIcons } from '../../../svg-icon'
 import { variants, type BannerIconVariants } from './banner-icon-variants'
 
-export type BannerNormalIconProps = {
-  type: Exclude<BannerIconVariants['type'], 'custom'>
-  children?: React.ReactNode
-}
-
-export type BannerCustomIconProps = {
-  type: 'custom'
+export type BannerIconProps = {
+  type: BannerIconVariants['type']
   children: React.ReactNode
 }
 
-export type BannerIconProps = BannerNormalIconProps | BannerCustomIconProps
-
 export function BannerIcon({ children, type = 'informative' }: BannerIconProps) {
-  const iconVariant: Record<Exclude<typeof type, 'custom'>, SvgIcons> = {
-    informative: 'bell',
-    positive: 'archive',
-    alert: 'circle-add',
-    error: 'info',
-  }
-
-  return (
-    <figure className={variants({ type })}>
-      {type === 'custom' ? children : <SvgIcon iconName={iconVariant[type]} />}
-    </figure>
-  )
+  return <figure className={variants({ type })}>{children}</figure>
 }
