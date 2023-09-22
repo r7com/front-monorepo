ff53259fdf1ba3096d3f43045c4259724c7dc910
rsmaciel <rsmaciel@recordtv.com.br>
2023-09-22 17:19:36 -0300

    refactor(admin): rename packages to @r7

--- apps/admin-e2e/cypress.config.ts
+++ apps/admin-e2e/cypress.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'cypress'
 import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset'
-import { configVisualRegressionDiff } from '@r7-jarvics/cypress'
+import { configVisualRegressionDiff } from '@r7/cypress'
 
 export default defineConfig({
   env: {
