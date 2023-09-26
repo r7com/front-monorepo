e8aa33181bc8dd5700f0d518fc12836a124dda4f
abcosta <abcosta@recordtv.com.br>
2023-09-26 10:12:20 -0300

    fix: rename namespace

--- libs/ui-footer-delivery/src/components/institutional/institutional-logo/institutional-logo.tsx
+++ libs/ui-footer-delivery/src/components/institutional/institutional-logo/institutional-logo.tsx
@@ -1,4 +1,4 @@
-import { SvgIcon } from '@r7-jarvics/ui-base-components'
+import { SvgIcon } from '@r7/ui-base-components'
 
 export function InstitucionalLogo() {
   return <SvgIcon iconName="logo-R7" title="Logo-R7" className="fill-neutral-high-400" />
