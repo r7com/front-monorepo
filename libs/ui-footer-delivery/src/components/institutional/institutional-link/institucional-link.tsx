a3b933a517f0c01f9b8fd1c3f84b514e5e1b8443
abcosta <abcosta@recordtv.com.br>
2023-09-27 12:41:45 -0300

    fix: added new props within components

--- libs/ui-footer-delivery/src/components/institutional/institutional-link/institucional-link.tsx
+++ libs/ui-footer-delivery/src/components/institutional/institutional-link/institucional-link.tsx
@@ -2,16 +2,24 @@ type InstitutionalLinkProps = {
   children: React.ReactNode
   url: string
   title: string
-}
+  openInNewtab?: boolean
+} & React.HTMLAttributes<HTMLAnchorElement>
 
-export function InstitutionalLink({ children, url, title }: InstitutionalLinkProps) {
+export function InstitutionalLink({
+  children,
+  url,
+  title,
+  openInNewtab = false,
+  ...rest
+}: InstitutionalLinkProps) {
   return (
     <a
       title={title}
       href={url}
-      target="_blank"
+      target={openInNewtab ? '_blank' : undefined}
       className="text-light-high-400 uppercase text-little hover:underline"
-      rel="noreferrer"
+      rel={openInNewtab ? 'noreferrer' : undefined}
+      {...rest}
     >
       {children}
     </a>
