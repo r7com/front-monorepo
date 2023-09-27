f124b18466fcdab7cc51a7b0639d91abf7f623cc
Alescia Fernandes <alfernandes@recordtv.com.br>
2023-09-27 11:30:26 -0300

    feat: card title component with dynamic tag

--- libs/ui-card/src/components/card/card-title/card-title.tsx
+++ libs/ui-card/src/components/card/card-title/card-title.tsx
@@ -1,3 +1,9 @@
-export function CardTitle() {
-  return <h3>Title</h3>
+import React from 'react'
+export type TitleProps<C extends React.ElementType> = {
+  children: React.ReactNode
+  as?: C
+}
+export function CardTitle<C extends React.ElementType = 'h3'>({ children, as }: TitleProps<C>) {
+  const DynamicTag = as || 'h3'
+  return <DynamicTag>{children}</DynamicTag>
 }
