e7a94bfc8f794a99c5fc988b60c1443ca562e468
rafaelfs96 <rafaelfs96@outlook.com>
2023-09-27 13:27:06 -0300

    style: ajuste nos estilos da lista de menu

--- libs/ui-header-delivery/src/components/menu/menu-item/menu-item.tsx
+++ libs/ui-header-delivery/src/components/menu/menu-item/menu-item.tsx
@@ -3,5 +3,9 @@ export type MenuItemProps = {
 }
 
 export function MenuItem({ children }: MenuItemProps) {
-  return <li className="relative h-lg inline-flex mr-xxxs uppercase">{children}</li>
+  return (
+    <li className="relative h-lg inline-flex mr-xxxs uppercase cursor-pointer items-center text-neutral-high-500 hover:text-neutral-high-400 hover:after:border-b-[3px] hover:after:absolute hover:after:w-full hover:after:inset-0 hover:after:content-['']">
+      {children}
+    </li>
+  )
 }
--- libs/ui-header-delivery/src/components/menu/menu-link/menu-link.tsx
+++ libs/ui-header-delivery/src/components/menu/menu-link/menu-link.tsx
@@ -6,7 +6,7 @@ export type MenuLinkProps = {
 export function MenuLink({ children, newWindow = true, ...rest }: MenuLinkProps) {
   return (
     <a
-      className="h-full flex items-center text-xxxs font-normal leading-5 text-neutral-high-500 hover:text-neutral-high-400 hover:after:border-b-[3px] hover:after:absolute hover:after:w-full hover:after:inset-0 hover:after:content-['']"
+      className="h-full flex items-center text-xxxs font-normal leading-5"
       target={newWindow ? '_blank' : '_self'}
       rel={newWindow ? 'noreferrer' : undefined}
       {...rest}
