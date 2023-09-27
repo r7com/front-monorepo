a71ed027c0ef2b48bebf5c5c42ce241900a8d344
rafaelfs96 <rafaelfs96@outlook.com>
2023-09-27 11:08:38 -0300

    style: ajuste na altura do header

--- libs/ui-header-delivery/src/components/header/header.variants.ts
+++ libs/ui-header-delivery/src/components/header/header.variants.ts
@@ -1,7 +1,7 @@
 import { createVariants } from '@r7/tools'
 
 export const variants = createVariants({
-  base: 'flex justify-around items-center px-nano h-auto w-full bg-editorial-color',
+  base: 'flex justify-around items-center px-nano h-lg w-full bg-editorial-color',
   variants: {
     fixed: {
       true: 'fixed',
