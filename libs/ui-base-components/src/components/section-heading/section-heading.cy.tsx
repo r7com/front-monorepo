6ca11ec80fe918ec6cc3963b40e9758e97dfd903
Jorge Santos <jldsantos@rederecord.com.br>
2023-10-02 16:11:33 -0300

    test: adiciona testes ao SectionHeading

--- libs/ui-base-components/src/components/section-heading/section-heading.cy.tsx
+++ libs/ui-base-components/src/components/section-heading/section-heading.cy.tsx
@@ -1,29 +1,100 @@
 import { SectionHeading } from './'
 
-describe(`${SectionHeading.Root.name} variants`, () => {
-  it('should render', () => {
-    cy.mount(
-      <SectionHeading.Root color="red">
-        <SectionHeading.Title>
-          <a href="/">Entretenimento</a>
-        </SectionHeading.Title>
+const SectionHeadingTitle = (
+  <SectionHeading.Root color="#F78714">
+    <SectionHeading.Title>
+      <a href="/">Entretenimento</a>
+    </SectionHeading.Title>
 
-        <SectionHeading.Line />
+    <SectionHeading.Line />
 
-        <SectionHeading.Tag href="#" title="Flavio Ricco">
-          Flavio Ricco
-        </SectionHeading.Tag>
+    <SectionHeading.Tags>
+      <SectionHeading.Tag href="#" title="Flavio Ricco">
+        Flavio Ricco
+      </SectionHeading.Tag>
 
-        <SectionHeading.Tag href="#" title="Keila Jimenez">
-          Keila Jimenez
-        </SectionHeading.Tag>
+      <SectionHeading.Tag href="#" title="Keila Jimenez">
+        Keila Jimenez
+      </SectionHeading.Tag>
 
-        <SectionHeading.Tag href="#" title="Fabíola Reipert">
-          Fabíola Reipert
-        </SectionHeading.Tag>
-      </SectionHeading.Root>,
-    )
-    cy.findByRole('status').should('be.visible')
+      <SectionHeading.Tag href="#" title="Fabíola Reipert">
+        Fabíola Reipert
+      </SectionHeading.Tag>
+    </SectionHeading.Tags>
+  </SectionHeading.Root>
+)
+
+const SectionHeadingImage = (
+  <SectionHeading.Root color="#4766ac">
+    <a href="/">
+      <SectionHeading.Image
+        src="https://img.r7.com/images/jr-24h-18112019115830166?resize=110x70&crop=200x127+0+37"
+        alt="Jornal da Record"
+      />
+    </a>
+
+    <SectionHeading.Line />
+
+    <SectionHeading.Tags>
+      <SectionHeading.Tag href="#" title="Christina Lemos">
+        Christina Lemos
+      </SectionHeading.Tag>
+
+      <SectionHeading.Tag href="#" title="Thiago Nolasco">
+        Thiago Nolasco
+      </SectionHeading.Tag>
+
+      <SectionHeading.Tag href="#" title="Luiz Fara Monteiro">
+        Luiz Fara Monteiro
+      </SectionHeading.Tag>
+    </SectionHeading.Tags>
+  </SectionHeading.Root>
+)
+
+describe(`SectionHeading`, () => {
+  it('should match desktop image with text as title', () => {
+    cy.viewport('macbook-11')
+    cy.mount(SectionHeadingTitle)
     cy.matchImage()
+  })
+
+  it('should match desktop image with image as title', () => {
+    cy.viewport('macbook-11')
+    cy.mount(SectionHeadingImage)
+    cy.matchImage()
+  })
+
+  it('should match mobile image with text as title', () => {
+    cy.viewport('iphone-se2')
+    cy.mount(SectionHeadingTitle)
+    cy.matchImage()
+  })
+
+  it('should match mobile image with image as title', () => {
+    cy.viewport('iphone-se2')
+    cy.mount(SectionHeadingImage)
+    cy.matchImage()
+  })
+
+  it('should have a title when title is text', () => {
+    cy.mount(SectionHeadingTitle)
+    cy.findByRole('heading').should('be.visible')
+  })
+
+  it('should have a image when title is image', () => {
+    cy.mount(SectionHeadingImage)
+    cy.findByRole('img').should('be.visible')
+  })
+
+  it('should have a separator', () => {
+    cy.mount(SectionHeadingTitle)
+    cy.findByRole('separator').should('be.visible')
+  })
+
+  it('should render its tags', () => {
+    cy.mount(SectionHeadingTitle)
+    cy.findByRole('link', { name: /Flavio Ricco/i }).should('be.visible')
+    cy.findByRole('link', { name: /Keila Jimenez/i }).should('be.visible')
+    cy.findByRole('link', { name: /Fabíola Reipert/i }).should('be.visible')
   })
 })
