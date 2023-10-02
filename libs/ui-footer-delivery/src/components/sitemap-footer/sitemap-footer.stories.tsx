86438e4c4b5be4244c71dded3a41271c13621c36
rsmaciel <rsmaciel@recordtv.com.br>
2023-10-02 19:56:53 -0300

    docs(ui-footer-delivery): improve show code doc

--- libs/ui-footer-delivery/src/components/sitemap-footer/sitemap-footer.stories.tsx
+++ libs/ui-footer-delivery/src/components/sitemap-footer/sitemap-footer.stories.tsx
@@ -2,14 +2,26 @@ import type { Meta, StoryObj } from '@storybook/react'
 import { SitemapFooter } from './index'
 import { sitemapData } from '../../mocks/sitemap'
 
-type RootDesktopProps = {
+const columns = sitemapData?.children[0]?.children
+
+type SitemapFooterProps = {
   textColor: string
 }
 
-const columns = sitemapData?.children[0]?.children
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
+const meta: Meta<SitemapFooterProps> = {
+  title: 'Footer/SitemapFooter',
+  tags: ['autodocs'],
+  subcomponents: {},
+  argTypes: {},
+}
 
-const RootDesktop = ({ textColor }: RootDesktopProps) => {
-  return (
+export default meta
+type Story = StoryObj<SitemapFooterProps>
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Desktop: Story = {
+  render: args => (
     <SitemapFooter.Root>
       <SitemapFooter.Container>
         {columns?.length > 0 &&
@@ -19,7 +31,7 @@ const RootDesktop = ({ textColor }: RootDesktopProps) => {
                 <SitemapFooter.MenuList key={column._id}>
                   {column?.children.map(section => (
                     <SitemapFooter.MenuItem key={section._id}>
-                      <SitemapFooter.MenuTitle textColor={textColor}>
+                      <SitemapFooter.MenuTitle textColor={args.textColor}>
                         {section.name}
                       </SitemapFooter.MenuTitle>
                       {section?.children.length > 0 && (
@@ -44,30 +56,14 @@ const RootDesktop = ({ textColor }: RootDesktopProps) => {
           )}
       </SitemapFooter.Container>
     </SitemapFooter.Root>
-  )
-}
-
-// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
-const meta: Meta<typeof RootDesktop> = {
-  title: 'Footer/SitemapFooter',
-  component: RootDesktop,
-  tags: ['autodocs'],
-  argTypes: {},
-}
-
-export default meta
-type Story = StoryObj<typeof RootDesktop>
-
-// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
-export const Desktop: Story = {
-  render: args => <RootDesktop {...args} />,
+  ),
   args: {
     textColor: '#218ee1',
   },
   parameters: {
     design: {
       type: 'figma',
-      url: 'https://www.figma.com/file/aAvhagIuoibmWIOFnUorUe/Sitemap?node-id=20%3A263&mode=dev',
+      url: 'https://www.figma.com/file/aAvhagIuoibmWIOFnUorUe/Sitemap?node-id=19%3A677&mode=dev',
       accessToken: 'figd_sHs6Ap894w4C-OAFls7tuq0fMMROyue-8zQJ8hRE',
     },
   },
