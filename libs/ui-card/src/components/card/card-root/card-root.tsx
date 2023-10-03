e0f66c6cc2b8c43e2dd0d24459434f1f1cd9d676
Alescia Fernandes <alfernandes@recordtv.com.br>
2023-10-03 14:51:51 -0300

    feat: added hat subcomponents to index; Adding news attributes to the card root

--- libs/ui-card/src/components/card/card-root/card-root.tsx
+++ libs/ui-card/src/components/card/card-root/card-root.tsx
@@ -1,7 +1,16 @@
 export type RootProps = {
   children: React.ReactNode
+  newsUrl: string
+  newsTitle: string
+  openInBlank?: boolean
 }
 
-export function CardRoot({ children }: RootProps) {
-  return <article>{children}</article>
+export function CardRoot({ children, newsUrl, newsTitle, openInBlank = false }: RootProps) {
+  return (
+    <article>
+      <a href={newsUrl} title={newsTitle} target={openInBlank ? '_blank' : undefined}>
+        {children}
+      </a>
+    </article>
+  )
 }
--- libs/ui-card/src/components/card/index.ts
+++ libs/ui-card/src/components/card/index.ts
@@ -1,11 +1,15 @@
-import { CardHat } from './card-hat/card-hat'
+import { CardHatImage } from './card-hat/card-hat-image'
+import { CardHatTitle } from './card-hat/card-hat-title'
+import { CardHatWrapper } from './card-hat/card-hat-wrapper'
 import { CardImage } from './card-image/card-image'
 import { CardLabel } from './card-label/card-label'
 import { CardRoot } from './card-root/card-root'
 import { CardTitle } from './card-title/card-title'
 
 export const Card = {
-  Hat: CardHat,
+  HatImage: CardHatImage,
+  HatTitle: CardHatTitle,
+  HatWrapper: CardHatWrapper,
   Image: CardImage,
   Label: CardLabel,
   Root: CardRoot,
