6031714c6a86404c65f4f9257be3f16f1d6f7c33
rafaelfs96 <rafaelfs96@outlook.com>
2023-10-04 09:31:18 -0300

    chore: reorganizing menu-item and menu-link props

--- libs/ui-header-delivery/src/components/menu/menu-link/menu-link.tsx
+++ libs/ui-header-delivery/src/components/menu/menu-link/menu-link.tsx
@@ -1,14 +1,18 @@
 export type MenuLinkProps = {
   newWindow?: boolean
   children: React.ReactNode
-} & React.AnchorHTMLAttributes<HTMLAnchorElement>
+  url: string
+  title: string
+} & Omit<React.AnchorHTMLAttributes<HTMLAnchorElement>, 'href'>
 
-export function MenuLink({ children, newWindow = true, ...rest }: MenuLinkProps) {
+export function MenuLink({ children, title, url, newWindow = false, ...rest }: MenuLinkProps) {
   return (
     <a
-      className="h-full flex items-center text-xxxs font-normal leading-5"
+      title={title}
+      href={url}
       target={newWindow ? '_blank' : undefined}
       rel={newWindow ? 'noreferrer' : undefined}
+      className="h-full flex items-center text-xxxs font-normal leading-5 hover:after:border-b-[3px] hover:after:absolute hover:after:w-full hover:after:inset-0 hover:after:content-['']"
       {...rest}
     >
       {children}
