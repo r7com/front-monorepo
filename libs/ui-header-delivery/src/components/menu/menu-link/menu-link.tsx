42b941e69f31f6e3fa7259af844c641f63899dee
rafaelfs96 <rafaelfs96@outlook.com>
2023-10-04 12:50:00 -0300

    refactor: using anchor default props in menu-link and sidebar-link

--- libs/ui-header-delivery/src/components/menu/menu-link/menu-link.tsx
+++ libs/ui-header-delivery/src/components/menu/menu-link/menu-link.tsx
@@ -1,15 +1,11 @@
 export type MenuLinkProps = {
   newWindow?: boolean
   children: React.ReactNode
-  url: string
-  title: string
-} & Omit<React.AnchorHTMLAttributes<HTMLAnchorElement>, 'href'>
+} & React.AnchorHTMLAttributes<HTMLAnchorElement>
 
-export function MenuLink({ children, title, url, newWindow = false, ...rest }: MenuLinkProps) {
+export function MenuLink({ children, title, newWindow = false, ...rest }: MenuLinkProps) {
   return (
     <a
-      title={title}
-      href={url}
       target={newWindow ? '_blank' : undefined}
       rel={newWindow ? 'noreferrer' : undefined}
       className="h-full flex items-center text-xxxs font-normal leading-5 hover:after:border-b-[3px] hover:after:absolute hover:after:w-full hover:after:inset-0 hover:after:content-['']"
--- libs/ui-header-delivery/src/components/sidebar/sidebar-link/sidebar-link.tsx
+++ libs/ui-header-delivery/src/components/sidebar/sidebar-link/sidebar-link.tsx
@@ -1,21 +1,11 @@
 export type SidebarLinkProps = {
   children: React.ReactNode
   newWindow?: boolean
-  title: string
-  url: string
-} & Omit<React.AnchorHTMLAttributes<HTMLAnchorElement>, 'href'>
+} & React.AnchorHTMLAttributes<HTMLAnchorElement>
 
-export function SidebarLink({
-  children,
-  title,
-  url,
-  newWindow = false,
-  ...rest
-}: SidebarLinkProps) {
+export function SidebarLink({ children, newWindow = false, ...rest }: SidebarLinkProps) {
   return (
     <a
-      title={`Ir para ${title}`}
-      href={url}
       target={newWindow ? '_blank' : undefined}
       rel={newWindow ? 'noreferrer' : undefined}
       className="w-full flex hover:underline"
