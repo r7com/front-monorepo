84e4113ceec925b021c70ebc982ed3287f8e890f
rafaelfs96 <rafaelfs96@outlook.com>
2023-10-04 15:39:39 -0300

    style: changing backdrop styles

--- libs/ui-header-delivery/src/components/sidebar/sidebar-root/sidebar-root.variants.ts
+++ libs/ui-header-delivery/src/components/sidebar/sidebar-root/sidebar-root.variants.ts
@@ -4,20 +4,17 @@ export const variants = createVariants({
   slots: {
     sidebarMenu:
       'w-[280px] bg-neutral-high-400 z-20 transition-all duration-200 absolute inset-0 overflow-x-hidden',
-    sidebarBackdrop: 'transition-all bg-dark-low-600 absolute inset-0 z-10',
-    sidebarContaner: 'w-full h-screen absolute overflow-hidden top-lg flex',
+    sidebarContaner: 'w-full h-screen absolute overflow-hidden top-lg flex transition-all',
   },
   variants: {
     isSidebarOpen: {
       true: {
         sidebarMenu: 'translate-x-0',
-        sidebarContaner: 'visible',
-        sidebarBackdrop: 'opacity-medium',
+        sidebarContaner: 'visible backdrop-brightness-75',
       },
       false: {
         sidebarMenu: '-translate-x-full',
         sidebarContaner: 'invisible',
-        sidebarBackdrop: 'opacity-transparent',
       },
     },
   },
