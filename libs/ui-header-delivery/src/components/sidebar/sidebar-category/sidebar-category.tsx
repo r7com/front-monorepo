e024f811e9aa1bf4092ed4e8fba3e2fbdebf779d
rafaelfs96 <rafaelfs96@outlook.com>
2023-10-04 19:37:56 -0300

    refactor: changing sidebar-category to li element

--- libs/ui-header-delivery/src/components/sidebar/sidebar-category/sidebar-category.tsx
+++ libs/ui-header-delivery/src/components/sidebar/sidebar-category/sidebar-category.tsx
@@ -5,9 +5,9 @@ export type SidebarCategoryProps = {
 
 export function SidebarCategory({ children, title }: SidebarCategoryProps) {
   return (
-    <div className="px-xxs pb-xxs mt-xxs border-b-hairline border-b-neutral-high-600/intense uppercase">
+    <li className="pb-xxs mb-xxs border-b-hairline border-b-neutral-high-600/intense uppercase">
       {title && <h4 className="text-[#333] text-xs leading-5 mb-xxs font-bold">{title}</h4>}
       {children}
-    </div>
+    </li>
   )
 }
