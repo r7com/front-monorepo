b46f850ce61c6bf6b828718c660d9e94f89d9766
rafaelfs96 <rafaelfs96@outlook.com>
2023-10-04 09:32:15 -0300

    feat: creating sidebar-toggle component

---
+++ libs/ui-header-delivery/src/components/sidebar/sidebar-toggle/sidebar-toggle.tsx
@@ -0,0 +1,33 @@
+import { variants } from './sidebar-toggle.variants'
+import { useSidebar } from '../utils/hooks/use-sidebar'
+
+export type SidebarToggleProps = {
+  children: React.ReactNode
+}
+
+export function SidebarToggle({ children }: SidebarToggleProps) {
+  const { sidebar } = useSidebar()
+
+  const toggleSidebar = () => sidebar.toggle(!sidebar.isOpen)
+
+  const { buttonSlot, iconSlot, firstLineSlot, secondLineSlot, thirdLineSlot } = variants({
+    isSidebarOpen: sidebar.isOpen,
+  })
+
+  return (
+    <button
+      type="button"
+      className={buttonSlot()}
+      color="ghost"
+      aria-expanded={sidebar.isOpen}
+      onClick={toggleSidebar}
+    >
+      <div className={iconSlot()}>
+        <span className={firstLineSlot()}></span>
+        <span className={secondLineSlot()}></span>
+        <span className={thirdLineSlot()}></span>
+      </div>
+      {children}
+    </button>
+  )
+}
