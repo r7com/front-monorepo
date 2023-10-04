9784d83f1dd16eacccfc2108234b3e67886d56f9
rafaelfs96 <rafaelfs96@outlook.com>
2023-10-04 09:30:07 -0300

    style: adjust header styles

--- libs/ui-header-delivery/src/components/header/header.variants.ts
+++ libs/ui-header-delivery/src/components/header/header.variants.ts
@@ -1,7 +1,7 @@
 import { createVariants } from '@r7/tools'
 
 export const variants = createVariants({
-  base: 'flex justify-around items-center px-nano h-lg w-full bg-editorial-color',
+  base: 'flex justify-around items-center px-nano h-lg w-full bg-editorial-color relative',
   variants: {
     fixed: {
       true: 'fixed',
