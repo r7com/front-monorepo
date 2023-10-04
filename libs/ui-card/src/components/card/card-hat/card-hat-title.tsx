8bb2b840e72cbc0de7e9f1af3c9a13741e5333c4
Alescia Fernandes <alfernandes@recordtv.com.br>
2023-10-04 16:42:32 -0300

    refactor(ui-card): correcting styles of card-hat-title subcomponent

--- libs/ui-card/src/components/card/card-hat/card-hat-title.tsx
+++ libs/ui-card/src/components/card/card-hat/card-hat-title.tsx
@@ -4,7 +4,7 @@ export type CardHatTitleProps = {
 
 export function CardHatTitle({ children }: CardHatTitleProps) {
   return (
-    <p className="text-xxxs font-open-sans text-neutral-low-500 dark:text-neutral-high-400">
+    <p className="text-xxxs font-open-sans text-neutral-low-400 dark:text-neutral-high-400 uppercase">
       {children}
     </p>
   )
