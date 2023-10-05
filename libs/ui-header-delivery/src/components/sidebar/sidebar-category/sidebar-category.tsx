9cc8a2b873449e4859bf593b3eee75ae8793d756
rafaelfs96 <rafaelfs96@outlook.com>
2023-10-05 09:44:41 -0300

    style: changing hardcoded color to preset color in sidebar-category

--- libs/ui-header-delivery/src/components/sidebar/sidebar-category/sidebar-category.tsx
+++ libs/ui-header-delivery/src/components/sidebar/sidebar-category/sidebar-category.tsx
@@ -6,7 +6,9 @@ export type SidebarCategoryProps = {
 export function SidebarCategory({ children, title }: SidebarCategoryProps) {
   return (
     <li className="pb-xxs mb-xxs border-b-hairline border-b-neutral-high-600/intense uppercase">
-      {title && <h4 className="text-[#333] text-xs leading-5 mb-xxs font-bold">{title}</h4>}
+      {title && (
+        <h4 className="text-neutral-low-500 text-xs leading-5 mb-xxs font-bold">{title}</h4>
+      )}
       {children}
     </li>
   )
