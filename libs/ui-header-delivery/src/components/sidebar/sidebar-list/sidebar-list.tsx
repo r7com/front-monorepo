8a5d2de809764902fd7c89fe0cf62bc8cc95d179
rafaelfs96 <rafaelfs96@outlook.com>
2023-10-05 21:45:32 -0300

    feat: adding custom label for sidebar-list

--- libs/ui-header-delivery/src/components/sidebar/sidebar-list/sidebar-list.tsx
+++ libs/ui-header-delivery/src/components/sidebar/sidebar-list/sidebar-list.tsx
@@ -1,7 +1,8 @@
 export type SidebarListProps = {
   children: React.ReactNode
+  label?: string
 }
 
-export function SidebarList({ children }: SidebarListProps) {
-  return <ul aria-label="menu">{children}</ul>
+export function SidebarList({ children, label }: SidebarListProps) {
+  return <ul aria-label={label ? `Menu para ${label}` : 'Menu'}>{children}</ul>
 }
