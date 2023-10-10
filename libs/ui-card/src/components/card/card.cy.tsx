735921f098f580278291bf390b09de3d29911431
Alescia Fernandes <alfernandes@recordtv.com.br>
2023-10-10 15:00:13 -0300

    test(ui-card): added test for default card rendering

--- libs/ui-card/src/components/card/card.cy.tsx
+++ libs/ui-card/src/components/card/card.cy.tsx
@@ -1,5 +1,17 @@
-describe('My First Test', () => {
-  it('Does not do much!', () => {
+import { Card } from './'
+
+describe(`${Card.name}`, () => {
+  it('should render default card', () => {
+    cy.mount(
+      <Card newsTitle="I am a news" newsUrl="https://www.r7.com">
+        <Card.Title>I am a news</Card.Title>
+      </Card>,
+    )
+    cy.findByRole('article')
+      .should('be.visible')
+      .within(() => {
+        cy.findByRole('link').should('be.visible')
+      })
     cy.matchImage()
   })
 })
