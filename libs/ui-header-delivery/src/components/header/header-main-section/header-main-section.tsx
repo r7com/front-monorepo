9189033393a9d72aea5e67942238625a89ff8cc3
rafaelfs96 <rafaelfs96@outlook.com>
2023-10-17 11:34:37 -0300

    chore: removing unused className prop from header-main-section

--- libs/ui-header-delivery/src/components/header/header-main-section/header-main-section.tsx
+++ libs/ui-header-delivery/src/components/header/header-main-section/header-main-section.tsx
@@ -3,25 +3,17 @@ import { variants } from './header-main-section.variants'
 
 export type HeaderMainSectionProps = {
   children: React.ReactNode
-  className?: string
   bgColor?: string
 }
 
-export function HeaderMainSection({
-  children,
-  className = '',
-  bgColor = '',
-}: HeaderMainSectionProps) {
+export function HeaderMainSection({ children, bgColor = '' }: HeaderMainSectionProps) {
   const [{ y: axisY }] = useWindowScroll()
 
   // O header fica 'fixed' depois de 50px na regra atual
   const isFixed = axisY !== null && axisY >= 50
 
   return (
-    <div
-      className={variants({ fixed: isFixed, className })}
-      style={{ backgroundColor: bgColor || undefined }}
-    >
+    <div className={variants({ fixed: isFixed })} style={{ backgroundColor: bgColor || undefined }}>
       {children}
     </div>
   )
