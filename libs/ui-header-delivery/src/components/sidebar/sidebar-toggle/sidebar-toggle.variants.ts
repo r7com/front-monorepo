f1f5bed26a179c241bc64af56088096a05880d3b
rafaelfs96 <rafaelfs96@outlook.com>
2023-10-18 09:48:39 -0300

    style: fixing sidebar-toggle button style

--- libs/ui-header-delivery/src/components/sidebar/sidebar-toggle/sidebar-toggle.variants.ts
+++ libs/ui-header-delivery/src/components/sidebar/sidebar-toggle/sidebar-toggle.variants.ts
@@ -2,7 +2,8 @@ import { createVariants } from '@r7/tools'
 
 export const variants = createVariants({
   slots: {
-    buttonSlot: 'flex items-center h-full gap-[1px] text-neutral-high-400',
+    buttonSlot:
+      'flex items-center h-full gap-[1px] text-neutral-high-400 absolute left-xxxs sm:static',
     iconSlot: 'flex flex-col w-xxxs h-xxxs mr-nano justify-around',
     firstLineSlot: 'bg-neutral-high-400 w-full h-[2px] rounded-sm transition-all',
     secondLineSlot: 'bg-neutral-high-400 w-full h-[2px] rounded-sm transition-all',
