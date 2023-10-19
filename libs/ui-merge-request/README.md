8f9f85aae290e94427242aabfc264bf8e673e7ee
rsmaciel <rsmaciel@recordtv.com.br>
2023-10-19 18:55:56 -0300

    feat(ui-merge): test description in merge request
    long description commit

---
+++ libs/ui-merge-request/README.md
@@ -0,0 +1,7 @@
+# ui-merge-request
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test ui-merge-request` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/ui-merge-request/src/lib/ui-merge-request.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface UiMergeRequestProps {}
+
+export function UiMergeRequest(props: UiMergeRequestProps) {
+  return (
+    <div>
+      <h1>Welcome to UiMergeRequest!</h1>
+    </div>
+  )
+}
+
+export default UiMergeRequest
