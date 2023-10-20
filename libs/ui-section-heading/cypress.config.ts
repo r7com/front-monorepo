b4d2770df28f3f28ae7c9f1bb414a64699e7aef3
Jorge Santos <jldsantos@rederecord.com.br>
2023-10-20 09:12:36 -0300

    chore(ui-section-heading): atualiza cypress.config.ts

--- libs/ui-section-heading/cypress.config.ts
+++ libs/ui-section-heading/cypress.config.ts
@@ -1,6 +1,16 @@
 import { nxComponentTestingPreset } from '@nx/react/plugins/component-testing'
 import { defineConfig } from 'cypress'
+import { initPlugin } from '@frsource/cypress-plugin-visual-regression-diff/plugins'
+import { configVisualRegressionDiff } from '@r7/cypress'
 
 export default defineConfig({
-  component: nxComponentTestingPreset(__filename, { bundler: 'vite' }),
+  env: {
+    ...configVisualRegressionDiff,
+  },
+  component: {
+    ...nxComponentTestingPreset(__filename, { bundler: 'vite' }),
+    setupNodeEvents(on, config) {
+      initPlugin(on, config)
+    },
+  },
 })
