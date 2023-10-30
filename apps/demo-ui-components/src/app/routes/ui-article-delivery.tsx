9fe6ef0d877cb7a9b6b5121efa80d3e8d1e60bf9
rafaelfs96 <rafaelfs96@outlook.com>
2023-10-30 10:47:58 -0300

    chore: add social-share component to demo-ui app

--- apps/demo-ui-components/src/app/routes/ui-article-delivery.tsx
+++ apps/demo-ui-components/src/app/routes/ui-article-delivery.tsx
@@ -1,22 +1,32 @@
-import { Signature } from '@r7/ui-article-delivery'
+import { Signature, SocialShare } from '@r7/ui-article-delivery'
 
 export function UiArticleDelivery() {
   return (
-    <Signature>
-      <Signature.Agency
-        url="https://agenciaestado.com.br"
-        name="Agência Estado - R7"
-        imageUrl="https://img.r7.com/images/logo-agencia-estado-09012020093353497?dimensions=50x50"
-      />
-      <Signature.Content>
-        <Signature.Info
-          author="Agência Estado"
-          color="#4766ac"
-          sectionName="r7"
-          sectionUrl="https://noticias.r7.com/agencia-estado"
+    <div className="container flex flex-col gap-xxxs sm:gap-0 sm:flex-row sm:justify-between">
+      <Signature>
+        <Signature.Agency
+          url="https://agenciaestado.com.br"
+          name="Agência Estado - R7"
+          imageUrl="https://img.r7.com/images/logo-agencia-estado-09012020093353497?dimensions=50x50"
         />
-        <Signature.Date published="2023-06-29T15:12:23Z" modified="2023-08-28T14:32:13.07Z" />
-      </Signature.Content>
-    </Signature>
+        <Signature.Content>
+          <Signature.Info
+            author="Agência Estado"
+            color="#4766ac"
+            sectionName="r7"
+            sectionUrl="https://noticias.r7.com/agencia-estado"
+          />
+          <Signature.Date published="2023-06-29T15:12:23Z" modified="2023-08-28T14:32:13.07Z" />
+        </Signature.Content>
+      </Signature>
+      <SocialShare.List>
+        <SocialShare.Item name="facebook" link="https://www.google.com" title="titulo" />
+        <SocialShare.Item name="linkedin" link="https://www.google.com" title="titulo" />
+        <SocialShare.Item name="twitter" link="https://www.google.com" title="titulo" />
+        <SocialShare.Item name="whatsapp" link="https://www.google.com" title="titulo" />
+        <SocialShare.Item name="googleNews" link="kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk" title="titulo" />
+        <SocialShare.Item name="share" link="kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk" title="titulo" />
+      </SocialShare.List>
+    </div>
   )
 }
