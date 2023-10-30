c5b7b013fa7b4871d00f5eeeda65866d2d24bb25
rafaelfs96 <rafaelfs96@outlook.com>
2023-10-30 11:11:31 -0300

    test: add tests with different component variants

--- libs/ui-article-delivery/src/components/social-share/social-share.cy.tsx
+++ libs/ui-article-delivery/src/components/social-share/social-share.cy.tsx
@@ -1,7 +1,7 @@
 import { SocialShare } from './social-share'
 
 describe(SocialShare.name, () => {
-  beforeEach(() => {
+  it('should render social-share links with primary color and square shape', () => {
     cy.mount(
       <SocialShare>
         <SocialShare.List>
@@ -14,9 +14,70 @@ describe(SocialShare.name, () => {
         </SocialShare.List>
       </SocialShare>,
     )
+
+    cy.findByTitle('facebook').should('exist')
+    cy.findByTitle('linkedin').should('exist')
+    cy.findByTitle('twitter').should('exist')
+    cy.findByTitle('whatsapp').should('exist')
+    cy.findByTitle('google-news').should('exist')
+    cy.findByTitle('share').should('exist')
+
+    cy.findAllByRole('listitem').should('be.visible').and('have.length', 6)
+    cy.matchImage()
   })
 
-  it('should render social-share links', () => {
+  it('should render social-share links with secondary color and rounded shape', () => {
+    cy.mount(
+      <div className="bg-neutral-low-600 h-screen">
+        <SocialShare>
+          <SocialShare.List>
+            <SocialShare.Item
+              color="secondary"
+              shape="circle"
+              name="facebook"
+              link="#facebook"
+              title="facebook"
+            />
+            <SocialShare.Item
+              color="secondary"
+              shape="circle"
+              name="linkedin"
+              link="#linkedin"
+              title="linkedin"
+            />
+            <SocialShare.Item
+              color="secondary"
+              shape="circle"
+              name="twitter"
+              link="#twitter"
+              title="twitter"
+            />
+            <SocialShare.Item
+              color="secondary"
+              shape="circle"
+              name="whatsapp"
+              link="#whatsapp"
+              title="whatsapp"
+            />
+            <SocialShare.Item
+              color="secondary"
+              shape="circle"
+              name="googleNews"
+              link="#googlenews"
+              title="google-news"
+            />
+            <SocialShare.Item
+              color="secondary"
+              shape="circle"
+              name="share"
+              link="#share"
+              title="share"
+            />
+          </SocialShare.List>
+        </SocialShare>
+      </div>,
+    )
+
     cy.findByTitle('facebook').should('exist')
     cy.findByTitle('linkedin').should('exist')
     cy.findByTitle('twitter').should('exist')
