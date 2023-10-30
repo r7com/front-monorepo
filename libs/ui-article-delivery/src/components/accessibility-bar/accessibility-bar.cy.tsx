13910daa4ce45b9ed32cd1bfffbb1b37a93cb621
abcosta <abcosta@recordtv.com.br>
2023-10-30 13:33:35 -0300

    chore: fix storybook and cypresss

--- libs/ui-article-delivery/src/components/accessibility-bar/accessibility-bar.cy.tsx
+++ libs/ui-article-delivery/src/components/accessibility-bar/accessibility-bar.cy.tsx
@@ -1,49 +1,45 @@
 import { AccessibilityBar } from './accessibility-bar'
-import { Audima } from '../audima'
 import { ContrastControl } from '../contrast-control'
 import { FontSizeControl } from '../font-size-control'
+import { ArticleProvider } from '../../utils/provider/article-provider'
 
 describe('AccessibilityBar', () => {
-  it('Should render AccessibilityBar', () => {
+  beforeEach(() =>
     cy.mount(
-      <AccessibilityBar>
-        <Audima />
-        <AccessibilityBar.List>
-          <AccessibilityBar.Item>
-            <ContrastControl />
-          </AccessibilityBar.Item>
-          <AccessibilityBar.Item>
-            <FontSizeControl.Increase />
-          </AccessibilityBar.Item>
-          <AccessibilityBar.Item>
-            <FontSizeControl.Decrease />
-          </AccessibilityBar.Item>
-        </AccessibilityBar.List>
-      </AccessibilityBar>,
-    )
+      <ArticleProvider>
+        <AccessibilityBar>
+          {/* AUDIMA */}
+          <iframe
+            aria-label="Pressione control e ponto para iniciar ou pausar a leitura do conteúdo. Audíma"
+            title="Audima-player"
+            id="audima-iframe"
+            width="100%"
+            height="50"
+            src={`https://audio.audima.co/iframe-later-thin-audima.html?skin=thin&amp;statistic=true`}
+            className="checked aud-message-received w-full -ml-nano md:-ml-auto mb-xxxs md:mb-0"
+          ></iframe>
 
-    // cy.matchImage()
+          <AccessibilityBar.List>
+            <AccessibilityBar.Item>
+              <ContrastControl />
+            </AccessibilityBar.Item>
+            <AccessibilityBar.Item>
+              <FontSizeControl.Increase />
+            </AccessibilityBar.Item>
+            <AccessibilityBar.Item>
+              <FontSizeControl.Decrease />
+            </AccessibilityBar.Item>
+          </AccessibilityBar.List>
+        </AccessibilityBar>
+      </ArticleProvider>,
+    ),
+  )
+  it('Should render AccessibilityBar', () => {
+    cy.matchImage()
   })
 
-  // it('Should render AccessibilityBar mobile', () => {
-  //   cy.mount(
-  //     <AccessibilityBar>
-  //       <Audima />
-  //       <AccessibilityBar.List>
-  //         <AccessibilityBar.Item>
-  //           <ContrastControl />
-  //         </AccessibilityBar.Item>
-  //         <AccessibilityBar.Item>
-  //           <FontSizeControl.Increase />
-  //         </AccessibilityBar.Item>
-  //         <AccessibilityBar.Item>
-  //           <FontSizeControl.Decrease />
-  //         </AccessibilityBar.Item>
-  //       </AccessibilityBar.List>
-  //     </AccessibilityBar>,
-  //   )
-
-  //   cy.viewport(320, 480)
-  //   cy.matchImage()
-  // })
+  it('Should render AccessibilityBar mobile', () => {
+    cy.viewport(320, 480)
+    cy.matchImage()
+  })
 })
