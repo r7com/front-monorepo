fd9670d991280169058e8e7996ecae2d3501da02
rafaelfs96 <rafaelfs96@outlook.com>
2023-10-31 15:27:27 -0300

    feat: link creation

---
+++ libs/ui-hotsite-delivery/src/components/hotsite-menu/hotsite-link/hotsite-link.tsx
@@ -0,0 +1,24 @@
+export type HotsiteLinkProps = {
+  openInNewTab?: boolean
+} & React.AnchorHTMLAttributes<HTMLAnchorElement>
+
+export function HotsiteLink({ children, openInNewTab = false, ...rest }: HotsiteLinkProps) {
+  /**
+  ────────────▄▄█▀─────▄
+  ────────▄▄▀▀▒▒▒▀████▀▄
+  ───▄▄▄▀▀▒▄▀▒▒▄▄██▀▒▒▒█
+  ▄▀▀░░░░░░▀███▀▀░░░░░░█
+  ▀███▄▄▄▄▄▄▄▄▄▄▄▄▄▄███▀
+  */
+
+  return (
+    <a
+      target={openInNewTab ? '_blank' : undefined}
+      rel={openInNewTab ? 'noreferrer' : undefined}
+      className="uppercase hover:text-[var(--section-color)]"
+      {...rest}
+    >
+      {children}
+    </a>
+  )
+}
