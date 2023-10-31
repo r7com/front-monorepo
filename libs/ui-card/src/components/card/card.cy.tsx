a2a745f72ebc640b54a1a602047b9bed7efb74c8
Alescia Fernandes <alfernandes@recordtv.com.br>
2023-10-31 09:12:05 -0300

    test(ui-card): restructuring the main test of card component with hat

--- libs/ui-card/src/components/card/card.cy.tsx
+++ libs/ui-card/src/components/card/card.cy.tsx
@@ -3,8 +3,17 @@ import { Card } from './'
 describe(Card.name, () => {
   it('should render default card', () => {
     cy.mount(
-      <Card newsTitle="I am a news" newsUrl="https://www.r7.com">
-        <Card.Title>I am a news</Card.Title>
+      <Card newsUrlTitle="I am a news" newsUrl="https://www.r7.com">
+        <Card.HatWrapper>
+          <Card.HatImage
+            imageSource="//img.r7.com/images/concurso-publico-14032022123440824?dimensions=128x128"
+            description="Human hand writting in a paper"
+          />
+          <Card.HatTitle>News section</Card.HatTitle>
+        </Card.HatWrapper>
+        <Card.Title as="h2" fontStyle="heading-level-1">
+          News title
+        </Card.Title>
       </Card>,
     )
     cy.findByRole('article')
