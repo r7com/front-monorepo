1dd4b261891997e5adbac7ad1ca7694f736ab829
Kelly Guimarans <kguimarans@recordtv.com.br>
2023-11-01 10:46:02 -0300

    feat(ui-section-foot): layout adjustment

--- libs/ui-section-foot/src/components/section-foot-link/section-foot-link.tsx
+++ libs/ui-section-foot/src/components/section-foot-link/section-foot-link.tsx
@@ -1,5 +1,3 @@
-import { SvgIcon } from '@r7/ui-base-components'
-
 export type SectionFootLinkProps = {
   children: React.ReactNode
 } & React.AnchorHTMLAttributes<HTMLAnchorElement>
@@ -7,14 +5,10 @@ export type SectionFootLinkProps = {
 export function SectionFootLink({ children, ...rest }: SectionFootLinkProps) {
   return (
     <a
-      className={`flex items-center gap-nano text-little uppercase px-[14px] py-[6px] bg-[var(--bg)] rounded-pill whitespace-nowrap z-10`}
+      className={`flex items-center gap-nano text-little uppercase px-[16px] py-[12px] bg-[var(--bg)] rounded-pill whitespace-nowrap z-10`}
       {...rest}
     >
       {children}
-      <SvgIcon
-        iconName="chevron-right"
-        className="w-[5px] h-[12px] min-w-max min-h-max m-0 fill-[var(--color)]"
-      />
     </a>
   )
 }
