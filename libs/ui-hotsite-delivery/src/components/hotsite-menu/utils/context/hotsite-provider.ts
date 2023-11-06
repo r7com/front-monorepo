2352f98162be73539953d8fdb3eb6db05b453550
rafaelfs96 <rafaelfs96@outlook.com>
2023-11-06 11:04:45 -0300

    chore: deleting ui-hotsite-delivery lib - migrating its components to ui-section-menu

--- libs/ui-hotsite-delivery/src/components/hotsite-menu/utils/context/hotsite-provider.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from 'react'
-
-type HotsiteContextProps = {
-  currentActive: string
-  show(id: string): void
-  hide(): void
-}
-
-export const HotsiteContext = createContext<HotsiteContextProps | null>(null)
