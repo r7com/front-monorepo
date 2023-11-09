22d8dda726845c289dac7837d364403826f84397
Alescia Fernandes <alfernandes@recordtv.com.br>
2023-11-09 10:44:05 -0300

    feat(ui-card): added link option to image and title subcomponents

--- libs/ui-card/src/components/card/card-image/card-image.tsx
+++ libs/ui-card/src/components/card/card-image/card-image.tsx
@@ -3,6 +3,10 @@ import { CardImageVariants, variants } from './card-image.variants'
 export type CardImageProps = {
   className?: string
   children: React.ReactNode
+  withLink?: boolean
+  newsUrl?: string
+  newsUrlTitle?: string
+  openInBlank?: boolean
 } & CardImageVariants
 
 export function CardImage({
@@ -10,6 +14,23 @@ export function CardImage({
   children,
   shadow = false,
   format = 'square',
+  withLink = false,
+  newsUrl,
+  newsUrlTitle,
+  openInBlank = false,
 }: CardImageProps) {
-  return <figure className={variants({ shadow, format, className })}>{children}</figure>
+  return withLink ? (
+    <figure className={variants({ shadow, format, className })}>
+      <a
+        href={newsUrl}
+        title={newsUrlTitle}
+        target={openInBlank ? '_blank' : undefined}
+        rel={openInBlank ? 'noreferrer' : undefined}
+      >
+        {children}
+      </a>
+    </figure>
+  ) : (
+    <figure className={variants({ shadow, format, className })}>{children}</figure>
+  )
 }
--- libs/ui-card/src/components/card/card-title/card-title.tsx
+++ libs/ui-card/src/components/card/card-title/card-title.tsx
@@ -4,6 +4,10 @@ export type CardTitleProps = {
   children: React.ReactNode
   as?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
   className?: string
+  withLink?: boolean
+  newsUrl?: string
+  newsUrlTitle?: string
+  openInBlank?: boolean
 } & CardTitleVariants
 
 export function CardTitle({
@@ -12,7 +16,23 @@ export function CardTitle({
   className = '',
   fontStyle = 'heading-level-2',
   color = 'low',
+  withLink = false,
+  newsUrl,
+  newsUrlTitle,
+  openInBlank = false,
 }: CardTitleProps) {
   const DynamicTag = as || 'h3'
-  return <DynamicTag className={variants({ fontStyle, color, className })}>{children}</DynamicTag>
+  return withLink ? (
+    <a
+      className="flex items-center"
+      href={newsUrl}
+      title={newsUrlTitle}
+      target={openInBlank ? '_blank' : undefined}
+      rel={openInBlank ? 'noreferrer' : undefined}
+    >
+      <DynamicTag className={variants({ fontStyle, color, className })}>{children}</DynamicTag>
+    </a>
+  ) : (
+    <DynamicTag className={variants({ fontStyle, color, className })}>{children}</DynamicTag>
+  )
 }
