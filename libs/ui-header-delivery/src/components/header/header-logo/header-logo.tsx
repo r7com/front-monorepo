2a3b1df07209caf4d8df721a53bb80383308a5b7
abcosta <abcosta@recordtv.com.br>
2023-11-09 17:41:51 -0300

    refactor: added isHome and new rules

--- libs/ui-header-delivery/src/components/header/header-logo/header-logo.tsx
+++ libs/ui-header-delivery/src/components/header/header-logo/header-logo.tsx
@@ -2,13 +2,19 @@ export type HeaderLogoProps = {
   link: string
   logoUrl: string
   alt: string
+  isHome?: boolean
 }
 
-export function HeaderLogo({ logoUrl, link, alt }: HeaderLogoProps) {
+export function HeaderLogo({ logoUrl, link, alt, isHome = false }: HeaderLogoProps) {
   return (
     <div className="sm:mr-sm sm:ml-xxs">
       <a href={link} title="Portal R7">
-        <img src={logoUrl} alt={alt} width={36} height={40} />
+        <img aria-labelledby="logo-r7com" src={logoUrl} alt={alt} width={36} height={40} />
+        {isHome && (
+          <h1 id="logo-r7com" className="sr-only">
+            R7.com
+          </h1>
+        )}
       </a>
     </div>
   )
