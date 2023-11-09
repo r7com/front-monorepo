6aed26f3be1e38b3c93177003141d9849f249c08
abcosta <abcosta@recordtv.com.br>
2023-11-09 16:59:55 -0300

    fix(header): removed lazy

--- libs/ui-header-delivery/src/components/header/header-logo/header-logo.tsx
+++ libs/ui-header-delivery/src/components/header/header-logo/header-logo.tsx
@@ -8,7 +8,7 @@ export function HeaderLogo({ logoUrl, link, alt }: HeaderLogoProps) {
   return (
     <div className="sm:mr-sm sm:ml-xxs">
       <a href={link} title="Portal R7">
-        <img src={logoUrl} loading="lazy" alt={alt} width={36} height={40} />
+        <img src={logoUrl} alt={alt} width={36} height={40} />
       </a>
     </div>
   )
