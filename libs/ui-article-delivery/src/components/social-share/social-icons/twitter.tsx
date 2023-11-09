7f6b09a5d33c11c49025def9e660e73c396ed1b7
rafaelfs96 <rafaelfs96@outlook.com>
2023-11-09 13:05:56 -0300

    feat(ui-article-delivery): share links using popup instead of default anchor

--- libs/ui-article-delivery/src/components/social-share/social-icons/twitter.tsx
+++ libs/ui-article-delivery/src/components/social-share/social-icons/twitter.tsx
@@ -1,16 +1,19 @@
+import { onMediaShare } from '../utils/sharer'
 import { variants } from './social-icons.variants'
 import { SocialIconProps } from './types'
-import { SvgIcon } from '@r7/ui-base-components'
+import { IconButton, SvgIcon } from '@r7/ui-base-components'
 
 export function TwitterIcon({ color, link, title }: SocialIconProps) {
   const { twitterSlot } = variants({ color })
 
   return (
-    <a
-      href={`https://twitter.com/share?url=${link}&text=${title}&hashtag=R7&via=portalr7`}
+    <IconButton
+      onClick={() =>
+        onMediaShare(`https://twitter.com/share?url=${link}&text=${title}&hashtag=R7&via=portalr7`)
+      }
       title="Compartilhe no Twitter"
     >
       <SvgIcon iconName="twitter" className={twitterSlot()} title="twitter" />
-    </a>
+    </IconButton>
   )
 }
---
+++ libs/ui-article-delivery/src/components/social-share/utils/sharer.ts
@@ -0,0 +1,3 @@
+export function onMediaShare(shareLink: string) {
+  window.open(shareLink, 'sharer', 'toolbar=0,status=0,width=626,height=436')
+}
