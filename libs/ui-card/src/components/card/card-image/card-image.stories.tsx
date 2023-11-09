35e8d65f60a4d70a13c444b55191bc260f094c47
Alescia Fernandes <alfernandes@recordtv.com.br>
2023-11-09 10:45:46 -0300

    docs(ui-card): added stories for link option of image and title subcomponents

--- libs/ui-card/src/components/card/card-image/card-image.stories.tsx
+++ libs/ui-card/src/components/card/card-image/card-image.stories.tsx
@@ -13,7 +13,7 @@ export default meta
 
 type Story = StoryObj<CardImageProps>
 
-export const WithoutLabel: Story = {
+export const Primary: Story = {
   render: ({ ...args }) => (
     <Card
       newsUrlTitle="The McRib is back (again): How a McNugget shortage led to its rise"
@@ -32,6 +32,36 @@ export const WithoutLabel: Story = {
     className: 'mb-xxxs',
     format: 'landscape',
     shadow: false,
+  },
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/Pxz9nVdP2Im0YRWnDNM3J5/Core-C-%7C-Widgets-Home?node-id=2039%3A624&mode=dev',
+      accessToken: 'figd_sHs6Ap894w4C-OAFls7tuq0fMMROyue-8zQJ8hRE',
+    },
+  },
+}
+
+export const WithLink: Story = {
+  render: ({ ...args }) => (
+    <Card withLink={false}>
+      <Card.Image {...args}>
+        <img
+          src="//img.r7.com/images/pantano-australia-rosa-brilhante-04102023182425856?resize=536x326&crop=691x420 80 0&dimensions=536x326"
+          alt="Human hand writting in a paper"
+          className="w-full object-cover h-full"
+        />
+        <Card.Label type="live" />
+      </Card.Image>
+    </Card>
+  ),
+  args: {
+    className: 'mb-xxxs',
+    format: 'landscape',
+    shadow: false,
+    withLink: true,
+    newsUrl: 'https://www.google.com',
+    newsUrlTitle: 'The McRib is back (again): How a McNugget shortage led to its rise',
   },
   parameters: {
     design: {
--- libs/ui-card/src/components/card/card-title/card-title.stories.tsx
+++ libs/ui-card/src/components/card/card-title/card-title.stories.tsx
@@ -25,7 +25,31 @@ export const Primary: Story = {
       </Card.Title>
     </Card>
   ),
-  args: {},
+  args: {
+    withLink: false,
+  },
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/Pxz9nVdP2Im0YRWnDNM3J5/Core-C-%7C-Widgets-Home?node-id=2039%3A624&mode=dev',
+      accessToken: 'figd_sHs6Ap894w4C-OAFls7tuq0fMMROyue-8zQJ8hRE',
+    },
+  },
+}
+
+export const WithLink: Story = {
+  render: ({ ...args }) => (
+    <Card className="flex" withLink={false}>
+      <Card.Title {...args}>
+        The McRib is back (again): How a McNugget shortage led to its rise
+      </Card.Title>
+    </Card>
+  ),
+  args: {
+    withLink: true,
+    newsUrl: 'https://www.google.com',
+    newsUrlTitle: 'The McRib is back (again): How a McNugget shortage led to its rise',
+  },
   parameters: {
     design: {
       type: 'figma',
