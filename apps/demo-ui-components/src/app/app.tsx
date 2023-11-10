cd04cf54923883b24643630876b68d9f51ead362
rafaelfs96 <rafaelfs96@outlook.com>
2023-11-10 14:10:30 -0300

    chore: adding header component demo-ui-components

--- apps/demo-ui-components/src/app/app.tsx
+++ apps/demo-ui-components/src/app/app.tsx
@@ -1,32 +1,37 @@
-import { Route, Routes, Link } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import { UiFooterDelivery, UiSectionMenu, UiArticleDelivery } from './routes'
 import { ArticleProvider } from '@r7/ui-article-delivery'
+import { Header, Menu } from '@r7/ui-header-delivery'
 
 export function App() {
   return (
     <ArticleProvider>
-      {/* START: routes */}
-      {/* These routes and navigation have been generated for you */}
-      {/* Feel free to move and update them to fit your needs */}
+      <Header>
+        <Header.MainSection>
+          <Header.Logo link="https://www.r7.com" image={'asdasda'} />
+
+          <Menu>
+            <Menu.List>
+              <Menu.Item>
+                <Menu.RouterLink to="/">Home</Menu.RouterLink>
+              </Menu.Item>
+              <Menu.Item>
+                <Menu.RouterLink to="/ui-footer-delivery">Footer Delivery</Menu.RouterLink>
+              </Menu.Item>
+              <Menu.Item>
+                <Menu.RouterLink to="/ui-article-delivery">Article Delivery</Menu.RouterLink>
+              </Menu.Item>
+              <Menu.Item>
+                <Menu.RouterLink to="/ui-section-menu">Section Menu</Menu.RouterLink>
+              </Menu.Item>
+            </Menu.List>
+          </Menu>
+        </Header.MainSection>
+      </Header>
       <br />
       <hr />
       <br />
-      <div role="navigation" className="mb-lg">
-        <ul className="list-disc list-inside">
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/ui-footer-delivery">Footer Delivery</Link>
-          </li>
-          <li>
-            <Link to="/ui-article-delivery">Ui Article Delivery</Link>
-          </li>
-          <li>
-            <Link to="/ui-section-menu">Section Menu</Link>
-          </li>
-        </ul>
-      </div>
+
       <Routes>
         <Route path="/" />
         <Route path="/ui-footer-delivery" element={<UiFooterDelivery />} />
