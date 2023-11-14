e54a01af60e391bca79778516187dc88fe698484
rsmaciel <rsmaciel@recordtv.com.br>
2023-11-14 16:53:46 -0300

    feat: update add prefix with support variants

--- add-prefix.js
+++ add-prefix.js
@@ -1,10 +1,21 @@
+/*
+  Insere o prefix em todas as classes do tailwind em arquivos TSX e variants
+  Fiz algumas melhorias no código do autor: https://github.com/tailwindlabs/tailwindcss/discussions/2598#discussioncomment-4813766
+  Precisava lidar com classes que tem os : antes da classe em si. Exemplo hover:flex. E também com !important.
+  Suporte estendido para dentro do arquivo de variants.
+*/
+
 const fs = require('fs')
 const path = require('path')
 const glob = require('glob')
-const rootDir = 'libs/ui-base-components'
-const classNameRegex = /className=[`']([\s\S]+?)[`']/g
+const prettier = require('prettier')
 
-glob('**/*.{ts,tsx}', { cwd: rootDir }, (er, files) => {
+const rootDir = 'libs/ui-article-delivery'
+const prefix = 'article'
+const variantKey = 'base'
+const classNameRegex = new RegExp(`(?:className=|${variantKey}:)\\s*["']([^"']+)["']`, 'g')
+
+glob('**/*.{variants.ts,tsx}', { cwd: rootDir }, (er, files) => {
   files.forEach(file => {
     const filePath = path.join(rootDir, file)
     fs.readFile(filePath, 'utf-8', (err, data) => {
@@ -12,20 +23,50 @@ glob('**/*.{ts,tsx}', { cwd: rootDir }, (er, files) => {
         console.error(err)
         return
       }
-      const result = data.replace(classNameRegex, (match, p1) => {
-        const classNames = p1.split(' ')
-        const prefixedClassNames = classNames.map(className => {
-          if (className.startsWith('base-')) {
-            return className
-          } else if (className.includes(':')) {
-            return className.replace(/(?<=:)([^;\s]+)/, 'base-$1')
-          } else {
-            return `base-${className}`
-          }
-        })
-        return `className="${prefixedClassNames.join(' ')}"`
+
+      const result = data.replace(classNameRegex, (match, classNames) => {
+        const updatedClassNames = classNames
+          .split(' ')
+          .map(className => {
+            const classHasBase = new RegExp(`(!?)${prefix}(?:-([^:]+))?$`)
+            const classWithoutBaseRx = /^((\w+):|(!)?)(\S+)$/
+
+            let includedPrefix = ''
+
+            if (classHasBase.test(className)) {
+              return className
+            }
+
+            if (className.match(classWithoutBaseRx)) {
+              const [fullClass, _, classWithColon, classWithImportant, finalClassName] =
+                className.match(classWithoutBaseRx)
+
+              if (classWithImportant) {
+                includedPrefix = `!${prefix}-${finalClassName}`
+              } else if (classWithColon) {
+                includedPrefix = finalClassName.includes('!')
+                  ? `${classWithColon}:!${prefix}-${finalClassName.replace('!', '')}`
+                  : `${classWithColon}:${prefix}-${finalClassName}`
+              } else {
+                includedPrefix = `${prefix}-${finalClassName}`
+              }
+            }
+
+            return includedPrefix
+          })
+          .join(' ')
+
+        const correctKey = match.startsWith('className=') ? 'className=' : `${variantKey}:`
+
+        return `${correctKey}"${updatedClassNames}"`
       })
-      fs.writeFile(filePath, result, 'utf-8', err => {
+
+      const typeContentWithPrettier = prettier.format(result, {
+        parser: 'typescript',
+        ...prettier.resolveConfig.sync(),
+      })
+
+      fs.writeFile(filePath, typeContentWithPrettier, 'utf-8', err => {
         if (err) console.error(err)
       })
     })
