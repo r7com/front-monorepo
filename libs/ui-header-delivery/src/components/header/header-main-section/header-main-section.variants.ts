e0f8c75a542336a27424156de68e520efc85e77a
rsmaciel <rsmaciel@recordtv.com.br>
2023-11-15 18:52:42 -0300

    fix(ui-header-delivery): add header prefix in z-index and top classes

--- libs/ui-header-delivery/src/components/header/header-main-section/header-main-section.variants.ts
+++ libs/ui-header-delivery/src/components/header/header-main-section/header-main-section.variants.ts
@@ -4,7 +4,7 @@ export const variants = createVariants({
   base: 'header-flex header-justify-around header-items-center header-px-nano header-h-lg header-w-full header-bg-editorial-color header-relative',
   variants: {
     fixed: {
-      true: '!fixed z-10 top-0',
+      true: '!header-fixed header-z-10 header-top-0',
     },
   },
 })
