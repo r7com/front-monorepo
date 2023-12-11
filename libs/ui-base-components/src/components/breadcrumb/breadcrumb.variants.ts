1db5554d265f96db169243ceefe04ff4430d684d
abcosta <abcosta@recordtv.com.br>
2023-12-11 16:45:21 -0300

    style: fix layout arrow

--- libs/ui-base-components/src/components/breadcrumb/breadcrumb.variants.ts
+++ libs/ui-base-components/src/components/breadcrumb/breadcrumb.variants.ts
@@ -1,7 +1,14 @@
 import { createVariants, VariantProps } from '@r7/tools'
 
 export const variants = createVariants({
-  base: "base-hidden base-text-xxxs base-text-neutral-low-400 last:base-flex first:base-flex md:base-flex after:base-content-['/'] after:base-block base-items-center base-capitalize after:base-mx-quark last:after:base-content-none last:base-font-bold",
+  base: `base-hidden base-text-little base-text-neutral-low-400 base-font-normal
+    last:base-flex first:base-flex md:base-flex after:base-border-t-hairline
+    after:base-content-[' '] after:base-w-[6px] after:base-h-[6px] after:base-rotate-45
+    after:base-cursor-pointer after:base-block base-items-center base-capitalize
+    after:base-border-r-hairline after:base-border-r-neutral-low-500
+    after:base-border-t-neutral-low-500 after:base-border-solid
+    after:base-mx-nano last:after:base-content-none
+    last:base-font-bold last:base-text-neutral-low-500`,
 })
 
 export type BreadcrumbVariants = VariantProps<typeof variants>
