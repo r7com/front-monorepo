07407d6086e3c9bf5a73d498e71a95f63129cc31
rafaelfs96 <rafaelfs96@outlook.com>
2023-12-12 17:56:50 -0300

    docs(ui-layout): creating storybook file for proportional-grids

---
+++ libs/ui-layout/src/components/proportional.stories.tsx
@@ -0,0 +1,58 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Proportional } from './proportional'
+
+const Block = () => (
+  <div className="layout-h-[280px] layout-bg-neutral-low-400 layout-items-center layout-justify-center layout-flex">
+    Large Main Block
+  </div>
+)
+
+const meta: Meta<typeof Proportional> = {
+  title: 'layout/Proportional',
+  tags: ['autodocs'],
+}
+
+export default meta
+
+type StoryOne = StoryObj<typeof Proportional.One>
+type StoryTwo = StoryObj<typeof Proportional.Two>
+type StoryThree = StoryObj<typeof Proportional.Three>
+type StoryFour = StoryObj<typeof Proportional.Four>
+
+export const ProportionalOne: StoryOne = {
+  render: () => (
+    <Proportional.One>
+      <Block />
+    </Proportional.One>
+  ),
+}
+
+export const ProportionalTwo: StoryTwo = {
+  render: () => (
+    <Proportional.Two>
+      <Block />
+      <Block />
+    </Proportional.Two>
+  ),
+}
+
+export const ProportionalThree: StoryThree = {
+  render: () => (
+    <Proportional.Three>
+      <Block />
+      <Block />
+      <Block />
+    </Proportional.Three>
+  ),
+}
+
+export const ProportionalFour: StoryFour = {
+  render: () => (
+    <Proportional.Four>
+      <Block />
+      <Block />
+      <Block />
+      <Block />
+    </Proportional.Four>
+  ),
+}
