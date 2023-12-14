23ab8b6b7c75adbd03cf452c5893a370a22caf32
rafaelfs96 <rafaelfs96@outlook.com>
2023-12-14 15:59:32 -0300

    test(ui-layout): tests and snapshots for magazine grids

---
+++ libs/ui-layout/src/components/magazine/magazine.cy.tsx
@@ -0,0 +1,70 @@
+import { Magazine } from './magazine'
+
+const Block = () => (
+  <div className="layout-h-[280px] layout-bg-neutral-low-400 layout-items-center layout-justify-center layout-flex">
+    Large Main Block
+  </div>
+)
+
+const MagazineOne = (
+  <Magazine.One>
+    <Block />
+    <Block />
+    <Block />
+  </Magazine.One>
+)
+
+const MagazineTwo = (
+  <Magazine.Two>
+    <Block />
+    <Block />
+  </Magazine.Two>
+)
+
+describe('Magazine Grids - Desktop', () => {
+  beforeEach(() => cy.viewport('macbook-11'))
+
+  it('should render magazine-one grid with two elements', () => {
+    cy.mount(MagazineOne)
+
+    cy.findAllByText(/Large Main Block/i)
+      .should('be.visible')
+      .and('have.length', 3)
+
+    cy.matchImage()
+  })
+
+  it('should render magazine-two grid with three elements', () => {
+    cy.mount(MagazineTwo)
+
+    cy.findAllByText(/Large Main Block/i)
+      .should('be.visible')
+      .and('have.length', 2)
+
+    cy.matchImage()
+  })
+})
+
+describe('Magazine Grids - Mobile', () => {
+  beforeEach(() => cy.viewport('iphone-se2'))
+
+  it('should render magazine-one grid with two elements', () => {
+    cy.mount(MagazineOne)
+
+    cy.findAllByText(/Large Main Block/i)
+      .should('be.visible')
+      .and('have.length', 3)
+
+    cy.matchImage()
+  })
+
+  it('should render magazine-two grid with three elements', () => {
+    cy.mount(MagazineTwo)
+
+    cy.findAllByText(/Large Main Block/i)
+      .should('be.visible')
+      .and('have.length', 2)
+
+    cy.matchImage()
+  })
+})
