a6976521a659e627e9f64f8971e41827d0dd3932
rafaelfs96 <rafaelfs96@outlook.com>
2023-12-14 10:40:40 -0300

    chore: change grid prop to cols

--- libs/ui-layout/src/components/proportional/proportional.tsx
+++ libs/ui-layout/src/components/proportional/proportional.tsx
@@ -2,26 +2,26 @@ import { Children } from 'react'
 import { Grid } from '../grid'
 
 type ProportionalsProps = {
-  grid: 1 | 2 | 3 | 4
+  cols: 1 | 2 | 3 | 4
   children: React.ReactNode
 }
 
-function getProportionalGrids({ grid }: Pick<ProportionalsProps, 'grid'>) {
-  const gridLayout = {
+function getProportionalGrids({ cols }: Pick<ProportionalsProps, 'cols'>) {
+  const colsLayout = {
     1: 'md:layout-col-span-24',
     2: 'md:layout-col-span-12',
     3: 'md:layout-col-span-8',
     4: 'md:layout-col-span-6',
-  }[grid]
+  }[cols]
 
   return ({ children }: Pick<ProportionalsProps, 'children'>) => {
     const blocks = Children.toArray(children)
 
     return (
-      blocks.length <= grid && (
+      blocks.length <= cols && (
         <Grid>
           {Children.map(blocks, block => (
-            <div className={gridLayout}>{block}</div>
+            <div className={colsLayout}>{block}</div>
           ))}
         </Grid>
       )
@@ -30,8 +30,8 @@ function getProportionalGrids({ grid }: Pick<ProportionalsProps, 'grid'>) {
 }
 
 export const Proportional = {
-  One: getProportionalGrids({ grid: 1 }),
-  Two: getProportionalGrids({ grid: 2 }),
-  Three: getProportionalGrids({ grid: 3 }),
-  Four: getProportionalGrids({ grid: 4 }),
+  One: getProportionalGrids({ cols: 1 }),
+  Two: getProportionalGrids({ cols: 2 }),
+  Three: getProportionalGrids({ cols: 3 }),
+  Four: getProportionalGrids({ cols: 4 }),
 }
