ee9bd83e97df7a17c922b7e5f2d27a214dec1eee
Jorge Santos <jldsantos@rederecord.com.br>
2023-12-27 09:06:20 -0300

    test(proportional-photo): adds tests for ProportionalPhoto card/widget

---
+++ libs/ui-card/src/components/proportional-photo/proportional-photo.cy.tsx
@@ -0,0 +1,32 @@
+import { ProportionalPhoto } from './proportional-photo'
+
+describe(ProportionalPhoto.name, () => {
+  beforeEach(() => {
+    cy.mount(
+      <ProportionalPhoto
+        renderImage={
+          <img
+            src="//img.r7.com/images/apagao-sao-paulo-1123-19122023115816951?resize=348x199&amp;crop=1170x669 0 32&amp;dimensions=328x184"
+            alt="Ministério da Justiça abre processo contra Enel por apagões"
+            width="100%"
+            height="auto"
+          />
+        }
+        hat="são paulo e rio de janeiro"
+        title="Ministério da Justiça abre processo contra Enel por apagões"
+      />,
+    )
+  })
+
+  it('should render proportional card - desktop', () => {
+    cy.viewport('macbook-16')
+    cy.findAllByRole('article').should('be.visible')
+    cy.matchImage()
+  })
+
+  it('should render proportional card - mobile', () => {
+    cy.viewport('iphone-se2')
+    cy.findAllByRole('article').should('be.visible')
+    cy.matchImage()
+  })
+})
