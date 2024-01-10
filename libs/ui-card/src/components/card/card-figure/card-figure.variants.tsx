e936598857fac7b31dd591e3585153274be6d019
Jorge Santos <jldsantos@rederecord.com.br>
2024-01-10 11:16:32 -0300

    style(card-figure): new custom format variant for handling individual widget aspect ratios

--- libs/ui-card/src/components/card/card-figure/card-figure.variants.tsx
+++ libs/ui-card/src/components/card/card-figure/card-figure.variants.tsx
@@ -7,6 +7,7 @@ export const variants = createVariants({
       square: 'card-aspect-square',
       landscape: 'card-aspect-landscape',
       portrait: 'card-aspect-portrait',
+      custom: '',
     },
     shadow: {
       true: 'after:card-absolute after:card-inset-0 after:card-pointer-events-none after:card-bg-gradient-to-b card-from-transparent card-from-50% card-to-neutral-low-600',
