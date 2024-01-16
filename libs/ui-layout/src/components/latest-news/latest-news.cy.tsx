9b32543f39f474f74348d932729a589e7bc8983b
abcosta <abcosta@recordtv.com.br>
2024-01-16 17:34:13 -0300

    refactor: removed sectionHeading

--- libs/ui-layout/src/components/latest-news/latest-news.cy.tsx
+++ libs/ui-layout/src/components/latest-news/latest-news.cy.tsx
@@ -1,27 +1,20 @@
 import '@r7/ui-section-heading/css'
 import { LatestNews } from './index'
 import { LargeMainBlock, SidebarBlock } from './mocks/mocks'
-import { SectionHeading } from '@r7/ui-section-heading'
 
 describe('Latest', () => {
   beforeEach(() => {
     cy.mount(
-      <>
-        <SectionHeading color="#4766ac">
-          <SectionHeading.Title>Últimas</SectionHeading.Title>
-          <SectionHeading.Line />
-        </SectionHeading>
-        <LatestNews
-          renderMain={<LargeMainBlock />}
-          renderSidebar={
-            <LatestNews.Sidebar
-              rowOne={<SidebarBlock />}
-              rowTwo={<SidebarBlock />}
-              rowThree={<SidebarBlock />}
-            />
-          }
-        />
-      </>,
+      <LatestNews
+        renderMain={<LargeMainBlock />}
+        renderSidebar={
+          <LatestNews.Sidebar
+            rowOne={<SidebarBlock />}
+            rowTwo={<SidebarBlock />}
+            rowThree={<SidebarBlock />}
+          />
+        }
+      />,
     )
   })
 
--- libs/ui-layout/src/components/latest-news/latest-news.stories.tsx
+++ libs/ui-layout/src/components/latest-news/latest-news.stories.tsx
@@ -1,25 +1,18 @@
 import type { Meta, StoryObj } from '@storybook/react'
 import { LatestNews } from './latest-news'
-import { SectionHeading } from '@r7/ui-section-heading'
 import { LargeMainBlock, SidebarBlock } from './mocks/mocks'
 
 const LatestNewsBlock = () => (
-  <>
-    <SectionHeading color="#4766ac">
-      <SectionHeading.Title>Últimas</SectionHeading.Title>
-      <SectionHeading.Line />
-    </SectionHeading>
-    <LatestNews
-      renderMain={<LargeMainBlock />}
-      renderSidebar={
-        <LatestNews.Sidebar
-          rowOne={<SidebarBlock />}
-          rowTwo={<SidebarBlock />}
-          rowThree={<SidebarBlock />}
-        />
-      }
-    />
-  </>
+  <LatestNews
+    renderMain={<LargeMainBlock />}
+    renderSidebar={
+      <LatestNews.Sidebar
+        rowOne={<SidebarBlock />}
+        rowTwo={<SidebarBlock />}
+        rowThree={<SidebarBlock />}
+      />
+    }
+  />
 )
 
 const meta: Meta<typeof LatestNewsBlock> = {
