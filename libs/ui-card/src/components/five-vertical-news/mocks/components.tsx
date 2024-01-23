726010dd2c441592890c14667c206a662b92db85
rafaelfs96 <rafaelfs96@outlook.com>
2024-01-23 11:49:00 -0300

    docs(ui-card): creating storybook file for five-vertical-news and mock components

---
+++ libs/ui-card/src/components/five-vertical-news/mocks/components.tsx
@@ -0,0 +1,11 @@
+export const LargeMainBlock = () => (
+  <div className="card-h-[320px] card-bg-neutral-low-400 card-items-center card-justify-center card-flex">
+    Large Main Block
+  </div>
+)
+
+export const SmallMainBlock = () => (
+  <div className="card-h-[240px] card-bg-neutral-low-400 card-items-center card-justify-center card-flex">
+    Small Main Block
+  </div>
+)
