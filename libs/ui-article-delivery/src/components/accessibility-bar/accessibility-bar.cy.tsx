e058a35284c38dd9bb67171274a3716ecabd9c87
abcosta <abcosta@recordtv.com.br>
2024-01-24 16:27:42 -0300

    chore: updated snapsshots

--- libs/ui-article-delivery/src/components/accessibility-bar/accessibility-bar.cy.tsx
+++ libs/ui-article-delivery/src/components/accessibility-bar/accessibility-bar.cy.tsx
@@ -9,17 +9,6 @@ describe('AccessibilityBar', () => {
       <div className="article-my-0 article-mx-auto article-w-[900px]">
         <ArticleProvider>
           <AccessibilityBar>
-            {/* AUDIMA */}
-            <iframe
-              aria-label="Pressione control e ponto para iniciar ou pausar a leitura do conteúdo. Audíma"
-              title="Audima-player"
-              id="audima-iframe"
-              width="100%"
-              height="50"
-              src="https://audio.audima.co/iframe-later-thin-audima.html?skin=thin&amp;statistic=true"
-              className="article-checked article-aud-message-received article-w-full -article-ml-nano md:-article-ml-auto article-mb-xxxs md:article-mb-0"
-            ></iframe>
-
             <AccessibilityBar.List>
               <AccessibilityBar.Item>
                 <ContrastControl />
@@ -36,7 +25,19 @@ describe('AccessibilityBar', () => {
       </div>,
     ),
   )
+
   it('Should render AccessibilityBar', () => {
+    cy.matchImage()
+  })
+
+  it('Should render AccessibilityBar dark mode', () => {
+    cy.addDarkMode()
+    cy.matchImage()
+  })
+
+  it('Should render AccessibilityBar Desktop dark mode', () => {
+    cy.addDarkMode()
+    cy.viewport('macbook-15')
     cy.matchImage()
   })
 
