07dc5b24c14074a5290dcbf74aac1c7f3e9c361f
Kelly Karoline Pereira Guimarans <kguimarans@recordtv.com.br>
2024-01-24 12:10:54 +0000

    feat(card): new left-photo variant

--- libs/ui-card/src/components/left-photo/left-photo-figure/left-photo-figure.tsx
+++ libs/ui-card/src/components/left-photo/left-photo-figure/left-photo-figure.tsx
@@ -1,12 +1,14 @@
 import { Card } from '../../card'
 import { CardFigureProps } from '../../card/card-figure/card-figure'
+import { LeftPhotoFigureVariants, variants } from './left-photo-figure.variants'
 
 export const LeftPhotoFigure = ({
   children,
+  imgSize,
   ...figureProps
-}: { children: React.ReactNode } & CardFigureProps) => {
+}: { children: React.ReactNode } & LeftPhotoFigureVariants & CardFigureProps) => {
   return (
-    <Card.Figure className="card-flex-shrink-0 card-h-fit card-mb-xxxs" {...figureProps}>
+    <Card.Figure className={variants({ imgSize })} {...figureProps}>
       {children}
     </Card.Figure>
   )
