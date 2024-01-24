e438eb720af6f1107ac0e5e0e80c573b3570f462
Jorge Luiz Dos Santos <jldsantos@recordtv.com.br>
2024-01-24 19:42:11 +0000

    style(guerra): new auto height based on the text/bullets size on tablet & mobile

--- libs/ui-card/src/components/guerra/guerra-title/guerra-title.tsx
+++ libs/ui-card/src/components/guerra/guerra-title/guerra-title.tsx
@@ -4,7 +4,7 @@ import { CardTitleProps } from '../../card/card-title/card-title'
 export const GuerraTitle = ({ children, ...cardTitleProps }: CardTitleProps) => {
   return (
     <Card.Title
-      className="card-text-md @[1000px]:card-text-xxl"
+      className="card-text-xl lg:card-text-xxl"
       color="high"
       fontStyle="none"
       {...cardTitleProps}
