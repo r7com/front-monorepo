d3d48f105d41424e32d2ddec52d84ce552b99a75
Jorge Santos <jldsantos@rederecord.com.br>
2024-01-24 08:43:27 -0300

    style(guerra): new auto height based on the text/bullets size on tablet & mobile
    We were using a fixed aspect-ratio that was overflowing the content vertically

--- libs/ui-card/src/components/guerra/guerra-bullets/guerra-bullets.tsx
+++ libs/ui-card/src/components/guerra/guerra-bullets/guerra-bullets.tsx
@@ -1,3 +1,7 @@
 export const GuerraBullets = ({ children }: { children: React.ReactNode }) => {
-  return <div className="card-grid card-grid-cols-2 card-gap-xxxs card-mt-xxxs">{children}</div>
+  return (
+    <div className="card-w-full card-grid card-grid-cols-2 card-gap-xxxs card-mt-xxxs">
+      {children}
+    </div>
+  )
 }
