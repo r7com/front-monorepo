d9683c9e4085ef6c69a5ca00c41e616259dc17ec
Anderson Batista da Costa <abcosta@recordtv.com.br>
2024-01-26 15:45:30 +0000

    Refactor/snapshots

--- libs/ui-article-delivery/src/components/signature/signature.cy.tsx
+++ libs/ui-article-delivery/src/components/signature/signature.cy.tsx
@@ -73,4 +73,28 @@ describe('Signature', () => {
     cy.findAllByTitle(/agência estado - r7/i).should('have.attr', 'rel', 'noreferrer')
     cy.matchImage()
   })
+
+  it('should render agencyImageUrl prop dark mode', () => {
+    cy.mount(
+      <Signature>
+        <Signature.Agency
+          url="https://agenciaestado.com.br"
+          name="Agência Estado - R7"
+          imageUrl="https://img.r7.com/images/logo-agencia-estado-09012020093353497?dimensions=50x50"
+        ></Signature.Agency>
+        <Signature.Content>
+          <Signature.Info
+            author="Agência Estado"
+            color="#4766ac"
+            sectionName="r7"
+            sectionUrl="https://noticias.r7.com/agencia-estado"
+          />
+          <Signature.Date published="2023-06-29T15:12:23Z" modified="2023-08-28T14:32:13.07Z" />
+        </Signature.Content>
+      </Signature>,
+    )
+
+    cy.addDarkMode()
+    cy.matchImage()
+  })
 })
