48b5c3e6dd3d2842e603516b425fac216a6b3c47
Jorge Santos <jldsantos@rederecord.com.br>
2024-01-26 14:52:05 -0300

    fix(section-heading-line): changes the border to a background to avoid the css overwrite on Arc

--- libs/ui-section-heading/src/components/section-heading/section-heading-line/section-heading-line.tsx
+++ libs/ui-section-heading/src/components/section-heading/section-heading-line/section-heading-line.tsx
@@ -1,5 +1,5 @@
 export function SectionHeadingLine() {
   return (
-    <hr className="sheading-w-full sheading-h-[1px] sheading-border-[var(--color)] sheading-pb-nano md:sheading-pb-0 dark:sheading-border-neutral-high-400" />
+    <hr className="sheading-w-full sheading-h-[1px] sheading-bg-[var(--color)] sheading-border-none sheading-mb-nano md:sheading-mb-0 dark:sheading-border-neutral-high-400" />
   )
 }
