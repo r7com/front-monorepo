fe3031b7d83916d6ce7e84a6fd8cb5f723d0b0b0
Jorge Santos <jldsantos@rederecord.com.br>
2024-01-26 18:33:34 -0300

    fix(dark-mode): fixes the dark-mode on the section-heading-line after the background change

--- libs/ui-section-heading/src/components/section-heading/section-heading-line/section-heading-line.tsx
+++ libs/ui-section-heading/src/components/section-heading/section-heading-line/section-heading-line.tsx
@@ -1,5 +1,5 @@
 export function SectionHeadingLine() {
   return (
-    <hr className="sheading-w-full sheading-h-[1px] sheading-bg-[var(--color)] sheading-border-none sheading-mb-nano md:sheading-mb-0 dark:sheading-border-neutral-high-400" />
+    <hr className="sheading-w-full sheading-h-[1px] sheading-bg-[var(--color)] sheading-border-none sheading-mb-nano md:sheading-mb-0 dark:sheading-bg-neutral-high-400" />
   )
 }
