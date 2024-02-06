235f2b41f43de8250e2043c0ff3a54e8755e1e02
abcosta <abcosta@recordtv.com.br>
2024-02-06 13:34:39 -0300

    refactor: changes color

--- libs/ui-card/src/components/most-read/most-read-title/most-read-title.tsx
+++ libs/ui-card/src/components/most-read/most-read-title/most-read-title.tsx
@@ -6,11 +6,7 @@ type MostReadTitleProps = {
 
 export function MostReadTitle({ children }: MostReadTitleProps) {
   return (
-    <Card.Title
-      as="h4"
-      fontStyle="none"
-      className="card-text-neutral-low-500 card-font-semibold card-mb-xxxs"
-    >
+    <Card.Title as="h4" fontStyle="none" color="low" className="card-font-semibold card-mb-xxxs">
       {children}
     </Card.Title>
   )
