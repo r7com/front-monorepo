15b174116249eae05366026ce70cc34b0a44f1d1
abcosta <abcosta@recordtv.com.br>
2024-02-06 13:33:44 -0300

    refactor: changes font styles

--- libs/ui-card/src/components/most-read/most-read-title/most-read-title.tsx
+++ libs/ui-card/src/components/most-read/most-read-title/most-read-title.tsx
@@ -1,7 +1,17 @@
+import { Card } from '../../card'
+
 type MostReadTitleProps = {
   children: React.ReactNode
 }
 
 export function MostReadTitle({ children }: MostReadTitleProps) {
-  return <h4 className="card-text-neutral-low-500 card-font-semibold card-mb-xxxs">{children}</h4>
+  return (
+    <Card.Title
+      as="h4"
+      fontStyle="none"
+      className="card-text-neutral-low-500 card-font-semibold card-mb-xxxs"
+    >
+      {children}
+    </Card.Title>
+  )
 }
