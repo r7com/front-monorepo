ac282c5278a259e848ad0449637e07300af68273
rafaelfs96 <rafaelfs96@outlook.com>
2024-02-21 14:15:06 -0300

    style(ui-tv-guide): fixing styles from tab button and select

--- libs/ui-tv-guide/src/components/tv-guide-programs/tabs/variants.ts
+++ libs/ui-tv-guide/src/components/tv-guide-programs/tabs/variants.ts
@@ -3,7 +3,7 @@ import { createVariants } from '@r7/ui-base-components'
 export const dayVariants = createVariants({
   slots: {
     buttonSlot:
-      'tv-w-xxxl md:tv-w-full tv-flex tv-justify-center tv-uppercase tv-text-xxxs tv-text-neutral-low-500',
+      'tv-w-xxxl md:tv-w-full tv-flex tv-font-bold tv-justify-center tv-uppercase tv-text-xxxs tv-text-neutral-low-500',
     itemSlot: 'tv-flex tv-flex-1 tv-bg-neutral-high-500',
   },
   variants: {
