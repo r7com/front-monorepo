98679063ed6e60952bf03991466b5858824b4b45
gshlima <gshlima@recordtv.com.br>
2024-03-01 13:56:36 -0300

    fix: adjusting the vertical bars

--- libs/ui-header-delivery/src/components/header/header-social-list/header-social-list.tsx
+++ libs/ui-header-delivery/src/components/header/header-social-list/header-social-list.tsx
@@ -4,14 +4,11 @@ export type HeaderSocialListProps = {
 
 export function HeaderSocialList({ children }: HeaderSocialListProps) {
   return (
-    <>
-      <ul
-        className="header-mr-quark header-hidden lg:header-flex header-h-full header-items-center"
-        aria-label="Redes sociais para compartilhar"
-      >
-        {children}
-      </ul>
-      <span className="header-bg-neutral-high-400 header-w-[1px] header-h-[25px] header-ml-nano header-mr-nano header-opacity-intense header-hidden lg:header-block"></span>
-    </>
+    <ul
+      className="header-mr-quark header-hidden lg:header-flex header-h-full header-items-center"
+      aria-label="Redes sociais para compartilhar"
+    >
+      {children}
+    </ul>
   )
 }
